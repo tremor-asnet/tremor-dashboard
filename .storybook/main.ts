2e23ccf09f424722c17363cc766b8152abe00f5a
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-12-04 16:50:13 +0700

    Feat: implement switch component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/addon-postcss",
+    "@storybook/addon-knobs"
   ],
   staticDirs: ["../public"],
   framework: {
---
+++ src/components/switch/switch.stories.tsx
@@ -0,0 +1,35 @@
+// Libs
+import type { Meta, StoryObj } from "@storybook/react";
+import { Switch } from "@tremor/react";
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+
+// Styles
+import './switch.css';
+
+const meta = {
+  title: "Components/Switch",
+  decorators: [withKnobs],
+  component: Switch,
+} as Meta<typeof Switch>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <Switch />
+  )
+};
+
+export const Checked: Story = {
+  render: () => (
+    <Switch checked/>
+  )
+};
+
+export const Disable: Story = {
+  render: () => (
+    <Switch disabled={boolean('Disabled', true)}/>
+  )
+};

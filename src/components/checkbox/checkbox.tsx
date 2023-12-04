30397138ef10695eb7b8958d45677eb670321bc4
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-12-04 10:25:10 +0700

    feat: update style for checkbox component

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -12,7 +12,13 @@ const CheckBox = ({
 }: InputProps) => {
   return (
     <label htmlFor={id}>
-      <input type="checkbox" id={id} {...props}/><span className="checkbox-layout"><span className="check"></span></span>
+      <input
+        className="opacity-0 absolute z-[-1] w-0 h-0 overflow-hidden pointer-events-none m-0 left-0"
+        type="checkbox" id={id} {...props}
+      />
+        <span className="checkbox-layout align-middle relative">
+        <span className="check relative inline-block w-5 h-5 border opacity-25 overflow-hidden z-[1] rounded-md border-solid border-[primary]">
+        </span></span>
     </label>
  );
 };

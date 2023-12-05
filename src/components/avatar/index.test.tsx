23dbfa0639acdfe7cc556ec723c2a83af8413529
Thao Pham <thao.phamduy@asnet.com.vn>
2023-12-05 15:17:41 +0700

    Feat/implement avatar component

---
+++ src/components/avatar/index.test.tsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+
+// Components
+import Avatar from "./";
+
+describe("Testing avatar component", () => {
+  const propsDefault = {
+    alt: 'Avatar md',
+    className: 'shadow-md',
+    height: 48,
+    priority: true,
+    src: '/images/avatar/avatar-md.webp',
+    width: 48,
+  };
+
+  it("Should match snapshot", () => {
+    const component = render(
+      <Avatar {...propsDefault} />
+    );
+    expect(component).toMatchSnapshot();
+  });
+});

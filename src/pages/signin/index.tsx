4515630abe120bb550c68ea985e7a4df6f420f65
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-12-05 16:19:25 +0700

    Feat: update style for signin and signup

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -2,10 +2,13 @@
 import { useState } from 'react';
 import { TextInput, Button, Flex, Switch, Text, Title } from '@tremor/react';
 import Link from 'next/link';
-import { FaGoogle, FaFacebook, FaGithub } from 'react-icons/fa';
+import { FaGoogle, FaFacebookSquare, FaGithub } from 'react-icons/fa';
 
 // Constant
 import { ROUTES } from '../../constants';
+
+// Styles
+import '../../components/switch/switch.css';
 
 const SignIn = () => {
 
@@ -18,35 +21,50 @@ const SignIn = () => {
 
   return (
     <Flex className='bg-[linear-gradient(195deg,rgba(66,66,74,0.6),rgba(25,25,25,0.6)),url("/assets/images/bg-sign-in.webp")] min-h-screen bg-cover bg-no-repeat bg-center'>
-      <Flex className='flex-col mx-auto bg-white rounded-xl max-w-sm'>
+      <Flex className='flex-col mx-auto bg-white rounded-xl max-w-sm sm:w-[350px]'>
         <div className='w-full p-4'>
-          <Flex className='bg-gradient-primary rounded-xl -mt-11 justify-center flex-col mb-7'>
-            <Title className='font-bold pt-8 pb-2 px-8 md:text-2xl text-white'>
-              Sign In
+          <Flex className='bg-gradient-primary rounded-xl -mt-11 justify-center flex-col mb-7 shadow-[rgba(52,71,103,0.15)_0rem_0.1875rem_0.1875rem_0rem,rgba(52,71,103,0.2)_0rem_0.1875rem_0.0625rem_-0.125rem,rgba(52,71,103,0.15)_0rem_0.0625rem_0.3125rem_0rem]'>
+            <Title className='font-medium pt-6 pb-2 px:8 md:text-2xl text-white'>
+              Sign in
             </Title>
-            <Flex className='my-8 max-w-xs justify-center space-x-3 px-4'>
-              <FaFacebook className='text-white' />
-              <FaGithub className='text-white' />
-              <FaGoogle className='text-white' />
+            <Flex className='my-8 pb-2 justify-center space-x-7 px-4'>
+              <Link
+                className='text-black-300 text-sm font-bold ml-2'
+                href={ROUTES.SINGIN}
+              >
+                <FaFacebookSquare className='text-white w-[17px] h-[17px]' />
+              </Link>
+              <Link
+                className='text-black-300 text-sm font-bold ml-2'
+                href={ROUTES.SINGIN}
+              >
+                <FaGithub className='text-white w-[18px] h-[18px]' />
+              </Link>
+              <Link
+                className='text-black-300 text-sm font-bold ml-2'
+                href={ROUTES.SINGIN}
+              >
+                <FaGoogle className='text-white w-[16px] h-[16px]' />
+              </Link>
             </Flex>
           </Flex>
           <div className='w-full p-3'>
-            <div className='h-20 w-full'>
+            <div className='h-16 w-full'>
               <TextInput
                 id='emmail'
                 placeholder='Email'
                 type='email'
                 autoFocus
-                className='py-1 w-full'
+                className='py-0.5 w-full'
               />
             </div>
-            <div className='h-20 w-full'>
+            <div className='h-16 w-full'>
               <TextInput
                 id='password'
                 placeholder='Password'
                 type='password'
                 autoFocus
-                className='py-1 w-full'
+                className='py-0.5 w-full'
               />
             </div>
             <div className='flex items-center space-x-3 mt-1'>
@@ -58,21 +76,21 @@ const SignIn = () => {
                 className='flex justify-center items-center'
                 onChange={handleSwitchChange}
               />
-              <Text className='text-gray-400'>Remember me</Text>
+              <Text className='text-secondary font-normal'>Remember me</Text>
             </div>
             <Button
-              className='w-full bg-gradient-primary py-3 mt-9 uppercase'
+              className='w-full font-normal bg-gradient-primary py-[9px] mt-9 uppercase border-transparent hover:border-transparent hover:shadow-[rgba(52,71,103,0.15)_0rem_0.1875rem_0.1875rem_0rem,rgba(52,71,103,0.2)_0rem_0.1875rem_0.0625rem_-0.125rem,rgba(52,71,103,0.15)_0rem_0.0625rem_0.3125rem_0rem]'
               size='xs'
             >
               Sign In
             </Button>
             <Flex className='mt-8 mb-2 justify-center items-center'>
-              <Text>Don&rsquo;t have an acccount?</Text>
+              <Text className='text-secondary font-light'>Don&rsquo;t have an acccount?</Text>
               <Link
-                className='text-black-300 text-sm font-bold ml-2'
+                className='text-black-300 font-semibold text-sm ml-2'
                 href={ROUTES.SIGNUP}
               >
-                Sign Up
+                Sign up
               </Link>
             </Flex>
           </div>

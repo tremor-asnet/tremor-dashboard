b2155867e0b3b1364a0b03bd32e84f1c3e1f1b6d
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-12-06 08:10:15 +0700

    feat: apply input component for signup form

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -7,6 +7,7 @@ import { ROUTES } from '../../constants';
 
 // Components
 import CheckBox from '../../components/checkbox/checkbox';
+import Input from '../../components/input/input';
 
 // Styles
 import '../../components/checkbox/checkbox.css';
@@ -14,44 +15,28 @@ import '../../components/checkbox/checkbox.css';
 const SignUp = () => {
   return (
     <div className='min-h-screen pb-8'>
-      <div className='bg-[linear-gradient(195deg,rgba(66,66,74,0.4),rgba(25,25,25,0.4)),url("/assets/images/bg-sign-up.webp")] m-4 pt-12 pb-60 opacity-100 rounded-xl shadow-none bg-cover bg-no-repeat bg-center w-[calc(100%-2rem)]'></div>
-      <Flex className='flex-col mx-auto bg-white rounded-xl max-w-sm -mt-32 shadow-dark-tremor-card'>
+      <div className='bg-[linear-gradient(195deg,rgba(66,66,74,0.4),rgba(25,25,25,0.4)),url("/assets/images/bg-sign-up.webp")] m-4 pt-12 pb-56 sm:pb-60 opacity-100 rounded-xl shadow-none bg-cover bg-no-repeat bg-center w-[calc(100%-2rem)]'></div>
+      <Flex className='flex-col w-[80%] sm:w-full mx-auto bg-white rounded-xl max-w-sm -mt-36 sm:-mt-32 shadow-dark-tremor-card'>
         <div className='w-full p-4'>
           <Flex className='bg-gradient-primary rounded-xl -mt-11 justify-center flex-col mb-7 pb-7 shadow-[rgba(52,71,103,0.15)_0rem_0.1875rem_0.1875rem_0rem,rgba(52,71,103,0.2)_0rem_0.1875rem_0.0625rem_-0.125rem,rgba(52,71,103,0.15)_0rem_0.0625rem_0.3125rem_0rem]'>
             <Title className='font-bold pt-8 pb-2 px-8 md:text-2xl text-white'>
               Join us today
             </Title>
-            <Text className='text-white font-light max-w-xs px-8 text-center'>
+            <Text className='text-white font-light max-w-xs px-6 sm:px-8 text-center'>
               Enter your email and password to register
             </Text>
           </Flex>
-          <div className='w-full p-3'>
-            <div className='h-16 w-full'>
-              <TextInput
-                id='name'
-                placeholder='Name'
-                type='text'
-                autoFocus
-                className='w-full sm:rounded-none sm:shadow-none border-0 border-b-2 hover:bg-transparent ring-0'
-              />
+          <div className='w-full sm:p-3'>
+            <div className="mb-4 w-full">
+              <Input variant='secondary' type='text' id="name" placeholder="Name" label="Name" />
             </div>
-            <div className="h-16 w-full">
-              <TextInput
-                id="email"
-                placeholder="Email"
-                type="email"
-                className='w-full sm:rounded-none sm:shadow-none border-0 border-b-2 boxhover:bg-transparent ring-0'
-              />
+            <div className="my-4 w-full">
+              <Input variant='secondary' type='email' id="email" placeholder="Email" label="Email" />
             </div>
-            <div className="h-16 w-full">
-              <TextInput
-                id="password"
-                placeholder="Password"
-                type="password"
-                className='w-full sm:rounded-none sm:shadow-none border-0 border-b-2 hover:bg-transparent ring-0'
-              />
+            <div className="my-4 w-full">
+              <Input variant='secondary' type='password' id="password" placeholder="Password" label="Password" />
             </div>
-            <div className='flex items-center space-x-3 mt-1'>
+            <div className='flex items-center space-x-3 mt-7'>
               <CheckBox id='checkbox'/>
               <Text className='text-secondary font-light'>
                 I agree with the{' '}

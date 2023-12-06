4eaf7acdb91d830e4bf29bc6ac346315ebad82bf
Son Xuan Son <son.phan@asnet.com.vn>
2023-12-06 16:28:21 +0700

    fix: responsive UI and style for card form

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -21,7 +21,7 @@ const SignIn = () => {
 
   return (
     <Flex className='bg-[linear-gradient(195deg,rgba(66,66,74,0.6),rgba(25,25,25,0.6)),url("/assets/images/bg-sign-in.webp")] min-h-screen bg-cover bg-no-repeat bg-center'>
-      <Flex className="flex-col mx-auto bg-white rounded-xl max-w-sm lg:max-w-[530px]">
+      <Flex className="flex-col mx-auto bg-white rounded-xl max-w-sm lg:max-w-xl">
         <div className="w-full p-4">
           <Flex className="bg-gradient-primary rounded-xl -mt-11 justify-center flex-col mb-7 shadow-[rgba(52,71,103,0.15)_0rem_0.1875rem_0.1875rem_0rem,rgba(52,71,103,0.2)_0rem_0.1875rem_0.0625rem_-0.125rem,rgba(52,71,103,0.15)_0rem_0.0625rem_0.3125rem_0rem]">
             <Title className="font-medium pt-6 pb-2 px:8 md:text-2xl text-white">
@@ -79,7 +79,7 @@ const SignIn = () => {
               <Text className="text-secondary font-normal">Remember me</Text>
             </div>
             <Button
-              className="w-full font-normal bg-gradient-primary py-[9px] mt-9 uppercase border-transparent hover:border-transparent hover:shadow-[rgba(52,71,103,0.15)_0rem_0.1875rem_0.1875rem_0rem,rgba(52,71,103,0.2)_0rem_0.1875rem_0.0625rem_-0.125rem,rgba(52,71,103,0.15)_0rem_0.0625rem_0.3125rem_0rem]"
+              className="w-full font-bold bg-gradient-primary py-[9px] mt-9 uppercase border-transparent hover:border-transparent hover:shadow-[rgba(52,71,103,0.15)_0rem_0.1875rem_0.1875rem_0rem,rgba(52,71,103,0.2)_0rem_0.1875rem_0.0625rem_-0.125rem,rgba(52,71,103,0.15)_0rem_0.0625rem_0.3125rem_0rem]"
               size="xs"
             >
               Sign In
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -16,7 +16,7 @@ const SignUp = () => {
   return (
     <div className="min-h-screen pb-8">
       <div className='bg-[linear-gradient(195deg,rgba(66,66,74,0.4),rgba(25,25,25,0.4)),url("/assets/images/bg-sign-up.webp")] m-4 pt-12 pb-56 sm:pb-60 opacity-100 rounded-xl shadow-none bg-cover bg-no-repeat bg-center w-[calc(100%-2rem)]'></div>
-      <Flex className="flex-col w-[80%] sm:w-full mx-auto bg-white rounded-xl max-w-sm -mt-36 sm:-mt-32 shadow-dark-tremor-card lg:max-w-[510px]">
+      <Flex className="flex-col w-[80%] sm:w-full mx-auto bg-white rounded-xl max-w-sm -mt-36 sm:-mt-32 shadow-dark-tremor-card lg:max-w-xl">
         <div className="w-full p-4">
           <Flex className="bg-gradient-primary rounded-xl -mt-11 justify-center flex-col mb-7 pb-7 shadow-[rgba(52,71,103,0.15)_0rem_0.1875rem_0.1875rem_0rem,rgba(52,71,103,0.2)_0rem_0.1875rem_0.0625rem_-0.125rem,rgba(52,71,103,0.15)_0rem_0.0625rem_0.3125rem_0rem]">
             <Title className="font-bold pt-8 pb-2 px-8 md:text-2xl text-white">
@@ -56,28 +56,28 @@ const SignUp = () => {
             </div>
             <div className="flex items-center space-x-3 mt-7">
               <CheckBox id="checkbox" />
-              <Text className="text-secondary font-light">
-                I agree with the{" "}
+              <Text className="text-secondary font-normal">
+                I agree the{" "}
                 <Link
                   href={ROUTES.HOME}
-                  className="hover:underline no-underline text-gray-800 text-sm font-semibold"
+                  className="hover:no-underline text-gray-800 text-sm font-bold"
                 >
                   Terms and conditions
                 </Link>
               </Text>
             </div>
             <Button
-              className="w-full font-normal bg-gradient-primary py-[9px] mt-9 uppercase border-transparent hover:border-transparent hover:shadow-[rgba(52,71,103,0.15)_0rem_0.1875rem_0.1875rem_0rem,rgba(52,71,103,0.2)_0rem_0.1875rem_0.0625rem_-0.125rem,rgba(52,71,103,0.15)_0rem_0.0625rem_0.3125rem_0rem]"
+              className="w-full font-bold bg-gradient-primary py-[9px] mt-9 uppercase border-transparent hover:border-transparent hover:shadow-[rgba(52,71,103,0.15)_0rem_0.1875rem_0.1875rem_0rem,rgba(52,71,103,0.2)_0rem_0.1875rem_0.0625rem_-0.125rem,rgba(52,71,103,0.15)_0rem_0.0625rem_0.3125rem_0rem]"
               size="xs"
             >
-              Sign Up
+              Sign In
             </Button>
             <Flex className="mt-8 mb-2 justify-center items-center">
               <Text className="text-secondary font-light">
                 Already have an acccount?
               </Text>
               <Link
-                className="text-black-300 font-semibold text-sm ml-2"
+                className="text-primary font-semibold text-sm ml-2"
                 href={ROUTES.SINGIN}
               >
                 Sign In

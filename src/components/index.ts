f5f70a3dc26cce130d16d1fd01fcde703eba78cc
Thao Pham <thao.phamduy@asnet.com.vn>
2023-12-08 11:40:01 +0700

    feat(40): implement dashboard header component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as CheckBox } from "./Checkbox/Checkbox";
 export { default as Input } from "./Input/Input";
 export { default as SignUpForm } from "./SignUpForm/SignUpForm";
 export { default as SignInForm } from "./SignInForm/SignInForm";
+export { default as DashboardHeader } from "./DashboardHeader/DashboardHeader";

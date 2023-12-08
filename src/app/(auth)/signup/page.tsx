0211905a3de5730e0ccc3cd2edc43042f2bfda27
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-12-08 13:06:58 +0700

    feat: remove social icon and update style for form signin, signup

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -3,8 +3,8 @@ import { SignUpForm } from "@/components";
 
 const SignUp = () => {
   return (
-    <div className="min-h-screen pb-8">
-      <div className='bg-[linear-gradient(195deg,rgba(66,66,74,0.4),rgba(25,25,25,0.4)),url("/assets/images/bg-sign-up.webp")] m-4 pt-12 pb-60 opacity-100 rounded-xl shadow-none bg-cover bg-no-repeat bg-center w-[calc(100%-2rem)]'></div>
+    <div className="bg-body min-h-screen pb-8 min-w-[320px]">
+      <div className='bg-[linear-gradient(195deg,rgba(66,66,74,0.4),rgba(25,25,25,0.4)),url("/assets/images/bg-sign-up.webp")] m-4 pt-12 pb-56 sm:pb-60 opacity-100 rounded-xl shadow-none bg-cover bg-no-repeat bg-center w-[calc(100%-2rem) min-h[35vh]'></div>
       <SignUpForm />
     </div>
   );

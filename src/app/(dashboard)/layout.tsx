0fad4726ae43dc4323ec28080c03ded545ed54b6
Hoang Nguyen <hoang.nguyenviethuy@asnet.com.vn>
2023-12-11 10:03:14 +0700

    feat(68): add sidebar to dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,13 +3,13 @@ import { ReactNode } from "react";
 
 // Components
 import { Flex } from "@tremor/react";
-import { DashboardHeader } from "@/components";
+import { DashboardHeader, SideBar } from "@/components";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <Flex alignItems="start" className="bg-body">
-      <div className="w-64 p-4 h-screen">
-        <div className="w-full h-full bg-black rounded-xl"></div>
+      <div className="p-4 h-screen">
+        <SideBar />
       </div>
       <div className="flex-1 p-4 pt-6">
         <DashboardHeader />

fd22ed2bab0e18c5eddb8dc045be5cf28d3a5fba
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-12-15 16:16:29 +0700

    feat(102): implement component project card

---
+++ src/constants/commons.ts
@@ -0,0 +1,29 @@
+export const ITEM_ACTION_PROJECT = [
+  {
+    key: "action1",
+    label: "Action",
+  },
+  {
+    key: "action2",
+    label: "Another action",
+  },
+  {
+    key: "action2",
+    label: "Something else here",
+  },
+];
+
+export const URL_AVATAR_PROJECT = [
+  {
+    key: "avatar1",
+    src: "/images/avatar/avatar-xs.webp",
+  },
+  {
+    key: "avatar2",
+    src: "images/avatar/avatar-xs.webp",
+  },
+  {
+    key: "avatar3",
+    src: "images/avatar/avatar-xs.webp",
+  },
+];

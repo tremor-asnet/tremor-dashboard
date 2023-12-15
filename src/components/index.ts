741103432eef0e54e0ad12c631c281921526d12a
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2023-12-15 14:48:27 +0700

    Merge branch 'sprint-3' into feat/implelent-ui-header-layout-profile-overview-page

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,7 +6,4 @@ export { default as DashboardHeader } from "./DashboardHeader/DashboardHeader";
 export { default as SideBar } from "./SideBar/SideBar";
 export { default as ProfileItem } from "./ProfileItem/ProfileItem";
 export { default as ProfileConversations } from "./ProfileConversations/ProfileConversations";
-<<<<<<< 3ca3edc12579a259cc2e60d5e49a893e3eb1d37c
 export { default as Tabs } from "./Tab/Tab";
-=======
->>>>>>> d772bdaeaa3bfb8375a1f9ce569a3486fac6c51d

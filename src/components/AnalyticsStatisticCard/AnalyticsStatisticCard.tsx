09f44ee5bd8d7923237ce0a5752b7a1c5fe0c9bc
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-12-22 14:50:20 +0700

    feat(151): apply component IconBox for AnalyticsStatisticCard component

--- src/components/AnalyticsStatisticCard/AnalyticsStatisticCard.tsx
+++ src/components/AnalyticsStatisticCard/AnalyticsStatisticCard.tsx
@@ -1,6 +1,9 @@
+//Libs
+import { ReactNode } from "react";
+
 //Components
 import { Card, Text, Flex } from "@tremor/react";
-import { IoMdPrint } from "react-icons/io";
+import IconBox from "@/components/IconBox/IconBox";
 
 //Mocks
 import { STATISTICAL_DATA } from "@/mocks/analytics";
@@ -14,10 +17,12 @@ type AnalyticsStatistical = {
 
 interface IAnalyticsStatisticCard {
   statisticalData: AnalyticsStatistical;
+  bgIcon?: string;
 }
 
 const AnalyticsStatisticCard = ({
   statisticalData = STATISTICAL_DATA[0],
+  bgIcon = "bg-[linear-gradient(195deg,#42424a,#191919)]",
 }: IAnalyticsStatisticCard): JSX.Element => {
   const { type, amount, totalAmount, duration } = statisticalData;
 
@@ -26,9 +31,10 @@ const AnalyticsStatisticCard = ({
       <div className="flex items-center">
         <Card className="mx-auto py-3 px-4 ring-0 max-w-full lg:max-w-[356px] 2xl:max-w-full border-none relative mt-[40px] rounded-xl shadow-md">
           <Flex className="">
-            <Flex className="absolute top-[-22px] left-40px w-[74px] h-[74px] p-1 bg-[linear-gradient(195deg,#42424a,#191919)] justify-center rounded-xl">
-              <IoMdPrint color="white" size="24px" />
-            </Flex>
+            <IconBox
+              className="absolute top-[-22px] left-40px w-[74px] h-[74px]"
+              bgBox={bgIcon}
+            />
             <Flex className="pl-[90px] flex-col mb-1 items-end">
               <Text className="text-md text-[#7b809a] font-light">{type}</Text>
               <Text className="text-[#344767] text-[24px] mt-3 font-bold">

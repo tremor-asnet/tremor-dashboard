5fb3dc71537a4dc5a2f17531de5c164388b02d12
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-12-22 10:04:39 +0700

    feat(139): implement analytics line chart

---
+++ src/mocks/charts.ts
@@ -0,0 +1,77 @@
+export const ANALYTICS_DAILY_CHART = [
+  {
+    date: "Apr",
+    "Desktop apps": 50,
+  },
+  {
+    date: "May",
+    "Desktop apps": 40,
+  },
+  {
+    date: "Jun",
+    "Desktop apps": 300,
+  },
+  {
+    date: "Jul",
+    "Desktop apps": 220,
+  },
+  {
+    date: "Aug",
+    "Desktop apps": 500,
+  },
+  {
+    date: "Sep",
+    "Desktop apps": 250,
+  },
+  {
+    date: "Oct",
+    "Desktop apps": 400,
+  },
+  {
+    date: "Nov",
+    "Desktop apps": 230,
+  },
+  {
+    date: "Dec",
+    "Desktop apps": 500,
+  },
+];
+
+export const ANALYTICS_TASK_CHART = [
+  {
+    date: "Apr",
+    "Mobile apps": 50,
+  },
+  {
+    date: "May",
+    "Mobile apps": 40,
+  },
+  {
+    date: "Jun",
+    "Mobile apps": 300,
+  },
+  {
+    date: "Jul",
+    "Mobile apps": 320,
+  },
+  {
+    date: "Aug",
+    "Mobile apps": 500,
+  },
+  {
+    date: "Sep",
+    "Mobile apps": 350,
+  },
+  {
+    date: "Oct",
+    "Mobile apps": 200,
+  },
+  {
+    date: "Nov",
+    "Mobile apps": 230,
+  },
+  {
+    date: "Dec",
+    "Mobile apps": 500,
+  },
+];

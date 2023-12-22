316a67d0b067f68853f6b2926943f384264c9b03
Van Nguyen <van.nguyenthi@asnet.com.vn>
2023-12-22 15:40:21 +0700

    feat(151): use width and height prop instead sise

--- src/components/IconBox/IconBox.stories.tsx
+++ src/components/IconBox/IconBox.stories.tsx
@@ -22,7 +22,8 @@ export const IconBoxSizeProps: Story = {
   render: () => (
     <IconBox
       icon={<MdLanguage color="white" size="24px" />}
-      size={{ width: "20", height: "20" }}
+      width="20"
+      height="20"
     />
   ),
 };
--- src/components/IconBox/IconBox.tsx
+++ src/components/IconBox/IconBox.tsx
@@ -4,28 +4,26 @@ import { ReactNode } from "react";
 //Components
 import { Flex } from "@tremor/react";
 import { IoMdPrint } from "react-icons/io";
-type size = {
-  width: string;
-  height: string;
-};
 
 interface IIconBox {
   icon?: ReactNode;
   bgBox?: string;
   className?: string;
-  size?: size;
+  width?: string;
+  height?: string;
 }
 
 const IconBox = ({
   icon = <IoMdPrint color="white" size="24px" />,
-  size = { width: "16", height: "16" },
+  width = "16",
+  height = "16",
   bgBox = "bg-[linear-gradient(195deg,#42424a,#191919)]",
   className = "",
 }: IIconBox): JSX.Element => {
   return (
     <Flex
-      width={size.width}
-      height={size.height}
+      width={width}
+      height={height}
       className={`p-1 justify-center rounded-xl ${bgBox} ${className}`}>
       {icon}
     </Flex>

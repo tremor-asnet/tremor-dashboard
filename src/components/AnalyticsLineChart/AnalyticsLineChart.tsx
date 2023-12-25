479747657dae8a1e0bd1fbbc0921d5880ff6c48a
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2023-12-25 13:59:43 +0700

    feat(105): remove unused code

--- src/components/AnalyticsLineChart/AnalyticsLineChart.tsx
+++ src/components/AnalyticsLineChart/AnalyticsLineChart.tsx
@@ -40,8 +40,6 @@ const AnalyticsLineChart = ({
   scheduleText,
   isDailyChart = true,
 }: LineChartProps) => {
-  // const [setValue] = useState<any>(null);
-
   return (
     <div className="w-full bg-white bg-clip-border shadow-[0rem_0.25rem_0.375rem_-0.0625rem_rgba(0,0,0,0.1),0rem_0.125rem_0.25rem_-0.0625rem_rgba(0,0,0,0.06)] overflow-visible h-full rounded-xl border-0 border-solid border-[rgba(0,0,0,0.125)]">
       <div className="p-4">
@@ -65,7 +63,6 @@ const AnalyticsLineChart = ({
                   ]}
                   yAxisWidth={30}
                   showAnimation={true}
-                  // onValueChange={(v: any) => setValue(v)}
                 />
               </Card>
             </div>

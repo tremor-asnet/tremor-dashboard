fca0ddb79a9a36e404adce051cec19f342617a4b
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-01-02 11:32:30 +0700

    feat(193): implement unit test

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -19,7 +19,7 @@ interface FormInputProps {
  * Primary UI component for Input component
  */
 const FormInput = ({
-  type = "text",
+  type,
   id,
   ariaDescribedby,
   placeholder,
@@ -28,32 +28,30 @@ const FormInput = ({
   disabled,
   errorMessage,
 }: FormInputProps): JSX.Element => {
-  // note TODO: using tailwind @apply
-  const inputPrimaryClasses = `block w-full p-3 text-sm text-tremor-content-title bg-transparent peer rounded-tremor-small outline outline-1 focus:outline-2 focus:outline-offset-[-1px] ${
-    !errorMessage
-      ? " outline-tremor-outline focus:outline-tremor-brand-subtle"
-      : " outline-red-600 focus:outline-red-600"
+  const labelPrimary =
+    "label-primary peer-placeholder-shown:scale-100 peer-focus:scale-75 peer-focus:px-2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:-translate-y-4";
+  const labelSecondary =
+    "label-secondary peer-placeholder-shown:scale-100 peer-focus:scale-75 peer-focus:start-0 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto";
+  const labelWithError = "text-red-600 peer-focus:text-red-600";
+  const labelWithoutError =
+    "text-tremor-content peer-focus:text-tremor-content-title";
+
+  const inputPrimaryClasses = `input-primary peer ${
+    !errorMessage ? "input-primary-without-error" : "input-primary-with-error"
   }`;
 
-  // note TODO: using tailwind @apply
-  const inputSecondaryClasses = `block w-full p-3 text-sm text-tremor-content-title bg-transparent peer ${
+  const inputSecondaryClasses = `input-secondary peer ${
     !errorMessage
-      ? " border-0 border-b focus:border-b-2 border-gray-300 focus:border-b-2 focus:outline-none focus:border-tremor-brand-subtle"
-      : " text-tremor-content-emphasis border-0 border-b focus:border-b-2 border-red-600 focus:outline-none focus:border-red-600"
+      ? "input-secondary-without-error"
+      : "input-secondary-with-error"
   }`;
 
-  // note TODO: using tailwind @apply
-  const labelPrimaryClasses = `absolute text-sm duration-300 transform peer-placeholder-shown:scale-100 scale-75 origin-[0] peer-focus:scale-75 -translate-y-4 top-2 z-10 bg-white px-2 peer-focus:px-2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:-translate-y-4 start-1 ${
-    !errorMessage
-      ? " text-tremor-content peer-focus:text-tremor-content-title"
-      : " text-red-600  peer-focus:text-red-600"
+  const labelPrimaryClasses = `${labelPrimary} ${
+    !errorMessage ? labelWithoutError : labelWithError
   }`;
 
-  // note TODO: using tailwind @apply
-  const labelSecondaryClasses = `absolute text-sm duration-300 transform peer-placeholder-shown:scale-100 scale-75 origin-[0] peer-focus:scale-75 -translate-y-6 top-3 -z-10 peer-focus:start-0 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto ${
-    !errorMessage
-      ? " text-tremor-content peer-focus:text-tremor-content-title"
-      : " text-red-600  peer-focus:text-red-600"
+  const labelSecondaryClasses = `${labelSecondary} ${
+    !errorMessage ? labelWithoutError : labelWithError
   }`;
 
   return (
@@ -73,7 +71,8 @@ const FormInput = ({
           htmlFor={id}
           className={`${
             variant === "primary" ? labelPrimaryClasses : labelSecondaryClasses
-          }`}>
+          }`}
+          data-testid="label">
           {label}
         </label>
       </div>

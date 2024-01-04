523067a7214ebc623069733f345bb3541f38bd33
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2024-01-04 15:31:26 +0700

    feat(203): Split config theme file into smaller files

---
+++ src/themes/metric.ts
@@ -0,0 +1,27 @@
+export const screens = {
+  xs: { min: "336px" },
+  // => @media (min-width: 336px) { ... }
+};
+
+export const borderRadius = {
+  "tremor-small": "0.375rem",
+  "tremor-default": "0.5rem",
+  "tremor-full": "9999px",
+  "tremor-none": "0",
+
+  //TODO: Will update when implement light/dark themes
+  // light mode
+  tremor: {
+    "tremor-small": "0.375rem",
+    "tremor-default": "0.5rem",
+    "tremor-full": "9999px",
+    "tremor-none": "0",
+  },
+  // dark mode
+  "dark-tremor": {
+    "tremor-small": "0.375rem",
+    "tremor-default": "0.5rem",
+    "tremor-full": "9999px",
+    "tremor-none": "0",
+  },
+};

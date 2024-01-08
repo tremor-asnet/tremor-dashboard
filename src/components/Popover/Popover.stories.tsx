ba16d176dcadab0b793b6adb228c1e8a86e23111
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-01-08 12:55:37 +0700

    feat(233): update export meta popover component

--- src/components/Popover/Popover.stories.tsx
+++ src/components/Popover/Popover.stories.tsx
@@ -4,12 +4,13 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Text, Flex } from "@tremor/react";
 import Popover from "./Popover";
 
-export default { component: Popover };
 const meta = {
   title: "Components/Popover",
   component: Popover,
   tags: ["autodocs"],
 } as Meta<typeof Popover>;
+
+export default meta;
 
 type Story = StoryObj<typeof meta>;
 

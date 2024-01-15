ca9497597d59a7fc01d428382e5dec1e7a8ce8f6
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2024-01-15 10:15:08 +0700

    feat(291): update middleware

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,9 @@ import { REMEMBER_ME } from "@/constants";
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$|.*\\.webp$).*)"],
+  matcher: [
+    "/((?!api|favicon.ico|sitemap.xml|robots.txt|_next/static|_next/image|.*\\.png$|.*\\.webp$).*)",
+  ],
 };
 
 export default function auth(params: GetServerSidePropsContext) {

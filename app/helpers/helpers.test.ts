5e923cae8c4e86f651e0750e7f01ad3ca0e9473f
duy.ngo <duy.ngo@asnet.com.vn>
2024-01-15 15:04:00 +0700

    feat(244): implement unit test

--- app/helpers/helpers.test.ts
+++ app/helpers/helpers.test.ts
@@ -1,4 +1,4 @@
-import { getErrorMessage, isEmpty } from ".";
+import { formatMoney, formatPercentage, getErrorMessage, isEmpty } from ".";
 
 describe("Test isEmpty function", () => {
   test("Check the param with empty value", () => {
@@ -41,5 +41,58 @@ describe("Test getErrorMessage function", () => {
   test("Should render correct message", () => {
     const result = getErrorMessage(404, "Not Found");
     expect(result).toBe("An error has occurred: 404 - Not Found");
+  });
+});
+
+describe("Test formatMoney function", () => {
+  test("formatMoney formats number correctly", () => {
+    const formattedMoney = formatMoney("23900");
+    expect(formattedMoney).toBe("$23,900");
+  });
+
+  test("formatMoney handles invalid input gracefully", () => {
+    const formattedMoney = formatMoney("invalidNumber");
+    expect(formattedMoney).toBe("$NaN");
+  });
+});
+
+describe("Test formatPercentage function", () => {
+  test("formatMoney formats number correctly", () => {
+    const formattedMoney = formatMoney("23900");
+    expect(formattedMoney).toBe("$23,900");
+  });
+
+  test("formatMoney handles invalid input gracefully", () => {
+    const formattedMoney = formatMoney("invalidNumber");
+    expect(formattedMoney).toBe("$NaN");
+  });
+});
+
+describe("Test formatPercentage function", () => {
+  test("formats positive number correctly", () => {
+    const input = "5.5";
+    const expectedOutput = "+5.5%";
+
+    const result = formatPercentage(input);
+
+    expect(result).toBe(expectedOutput);
+  });
+
+  test("formats zero correctly", () => {
+    const input = "0";
+    const expectedOutput = "0%";
+
+    const result = formatPercentage(input);
+
+    expect(result).toBe(expectedOutput);
+  });
+
+  test("formats negative number correctly", () => {
+    const input = "-3";
+    const expectedOutput = "-3%";
+
+    const result = formatPercentage(input);
+
+    expect(result).toBe(expectedOutput);
   });
 });

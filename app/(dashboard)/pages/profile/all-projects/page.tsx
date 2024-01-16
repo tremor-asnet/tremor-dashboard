5bbb40f5263f9501af210bc2208ab2f606a8a9f8
Thao Pham Duy <thao.phamduy@asnet.com.vn>
2024-01-16 16:57:10 +0700

    fix(314): update profile owner card is not have border

--- app/(dashboard)/pages/profile/all-projects/page.tsx
+++ app/(dashboard)/pages/profile/all-projects/page.tsx
@@ -25,7 +25,7 @@ const Projects = async () => {
     <div>
       <div className='bg-[linear-gradient(195deg,rgba(73,163,241,0.6),rgba(26,115,232,0.6)),url("/images/backgrounds/bg-profile.webp")] bg-cover bg-no-repeat bg-center min-h-[300px] pb-8 md:min-w-[320px] overflow-hidden p- rounded-xl'></div>
       <div className="mx-6 -mt-16">
-        <Card className="px-4 py-2 dark:bg-dark-tremor-primary ring-0 !border-transparent">
+        <Card className="px-4 py-2 dark:bg-dark-tremor-primary ring-0">
           <Flex className="flex-col md:flex-row items-start md:items-center">
             {/* TODO: Will update when have profile header component */}
             {/* Header */}

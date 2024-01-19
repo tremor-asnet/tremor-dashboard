5efc3f9b8734771ca9baa20e1a56db877cd76839
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2024-01-19 16:47:53 +0700

    feat(350): implement order list page

---
+++ app/(dashboard)/order-list/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+// Components
+import { TableList } from "@/components";
+import OrderSearch from "@/components/OrderSearch/OrderSearch";
+import { Button, Flex, Text } from "@tremor/react";
+
+// Icons
+import { RiArrowDropDownLine } from "react-icons/ri";
+
+// Mocks
+import { TABLE_LIST_DATA } from "@/mocks";
+
+const OrderListPage = () => {
+  const handleSearch = () => {
+    // TODO: Handle search here
+  };
+
+  const handleCheckBox = () => {
+    // TODO: Handle check box here
+  };
+
+  return (
+    <Flex flexDirection="col" className="gap-4">
+      <Flex>
+        <Button className="py-3 px-5 bg-gradient-primary dark:bg-gradient-pickled border-none dark:text-white">
+          <Text className="uppercase text-xs text-white dark:text-white">
+            new order
+          </Text>
+        </Button>
+        <Button
+          icon={RiArrowDropDownLine}
+          iconPosition="right"
+          className="font-bold bg-transparent border-primary focus:border-primary hover:border-primary focus:opacity-75 hover:opacity-75 text-primary focus:text-white dark:text-dark-tremor-content-title hover:bg-transparent active:bg-primary focus:bg-primary rounded-lg hover:!shadow-btn-primary-hover dark:border-primary dark:bg-transparent dark:hover:border-primary dark:hover:bg-transparent">
+          <Text className="uppercase text-xs">Filters</Text>
+        </Button>
+      </Flex>
+      <div className="w-full bg-white rounded-lg dark:bg-dark-tremor-primary">
+        <OrderSearch onSearch={handleSearch} />
+        <TableList data={TABLE_LIST_DATA} handleCheckBox={handleCheckBox} />
+      </div>
+    </Flex>
+  );
+};
+
+export default OrderListPage;

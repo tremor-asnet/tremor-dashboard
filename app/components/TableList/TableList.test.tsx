45e5f916fe48ec3f6dabbe15373599f9ccb918bb
Thao Pham Duy <thao.phamduy@asnet.com.vn>
2024-01-19 10:42:50 +0700

    feat(320): update unit test

--- app/components/TableList/TableList.test.tsx
+++ app/components/TableList/TableList.test.tsx
@@ -12,20 +12,6 @@ describe("Testing table list component", () => {
     data: TABLE_LIST_DATA,
   };
 
-  it("testing component table list render", () => {
-    const { container } = render(<TableList {...propsDefault} />);
-    const status = container.getElementsByClassName("order-status")[0];
-    const customer = container.getElementsByClassName("order-customer")[0];
-    const product = container.getElementsByClassName("order-product")[0];
-    const revenue = container.getElementsByClassName("order-revenue")[0];
-
-    expect(container).toMatchSnapshot();
-    expect(status).toBeInTheDocument();
-    expect(customer).toBeInTheDocument();
-    expect(product).toBeInTheDocument();
-    expect(revenue).toBeInTheDocument();
-  });
-
   it("Should match snapshot", () => {
     const component = render(<TableList {...propsDefault} />);
     expect(component).toMatchSnapshot();

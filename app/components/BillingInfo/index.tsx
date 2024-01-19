b20e2a19d27fff729c6737575e8f49e3598b32c0
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-01-19 09:21:21 +0700

    feat(330): create folder billing information

---
+++ app/components/BillingInfo/index.tsx
@@ -0,0 +1,9 @@
+import { Bold } from "@tremor/react";
+
+const BillingInfo = () => {
+  <div>
+    <Bold className="dark:text-white">Order Details</Bold>
+  </div>;
+};
+
+export default BillingInfo;

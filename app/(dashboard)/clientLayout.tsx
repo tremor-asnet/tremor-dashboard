8d1d481f11a7ffc28b77ccfbdcb0f02b98fcfeed
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2024-01-22 11:13:03 +0700

    feat(331): update logout function

--- app/(dashboard)/clientLayout.tsx
+++ app/(dashboard)/clientLayout.tsx
@@ -2,14 +2,17 @@
 
 // Libs
 import { ReactNode, useState } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 // Components
 import { Flex } from "@tremor/react";
-import { SideBar, DashboardHeader } from "@/components";
+import { SideBar, DashboardHeader, LoadingIndicator } from "@/components";
 
 // Actions
-import { signOutAction } from "@/actions";
+import { signOut } from "@/auth";
+
+// Constants
+import { ROUTES } from "@/constants";
 
 interface DashboardLayoutProp {
   profileData: {
@@ -23,7 +26,9 @@ export default function DashboardLayout({
   profileData,
   children,
 }: DashboardLayoutProp) {
+  const [isPending, setIsPending] = useState(false);
   const [isCollapseSidebar, setIsCollapseSidebar] = useState(false);
+  const router = useRouter();
   const pathname = usePathname();
 
   const toggleSidebar = () => {
@@ -32,28 +37,41 @@ export default function DashboardLayout({
 
   const { avatar, name } = profileData;
 
+  const signOutAction = async () => {
+    setIsPending(true);
+    await signOut({ redirect: false });
+    setIsPending(false);
+    setIsCollapseSidebar(false);
+    router.replace(ROUTES.SIGN_IN);
+  };
+
   return (
-    <Flex
-      alignItems="start"
-      className="bg-body dark:bg-dark-primary antialiased font-primary min-h-screen">
-      <SideBar
-        avatarUrl={avatar}
-        name={name}
-        pathname={pathname}
-        isCollapse={isCollapseSidebar}
-        toggleSidebar={toggleSidebar}
-        onSignOut={signOutAction}
-      />
-      <div
-        className={`flex flex-col w-full min-h-screen p-4 sm:p-5 md:p-6 pt-6 transition-all duration-300 ease-[cubic-bezier(0.4,0,0.6,1)] delay-20 ${
-          isCollapseSidebar ? "ml-0 xl:ml-28" : "xl:ml-[270px]"
-        }`}>
-        <DashboardHeader
+    <>
+      <Flex
+        alignItems="start"
+        className="bg-body dark:bg-dark-primary antialiased font-primary min-h-screen">
+        <SideBar
+          avatarUrl={avatar}
+          name={name}
+          pathname={pathname}
+          isCollapse={isCollapseSidebar}
           toggleSidebar={toggleSidebar}
-          isCollapseSidebar={isCollapseSidebar}
+          onSignOut={signOutAction}
         />
-        {children}
-      </div>
-    </Flex>
+        <div
+          className={`flex flex-col w-full min-h-screen p-4 sm:p-5 md:p-6 pt-6 transition-all duration-300 ease-[cubic-bezier(0.4,0,0.6,1)] delay-20 ${
+            isCollapseSidebar ? "ml-0 xl:ml-28" : "xl:ml-[270px]"
+          }`}>
+          <DashboardHeader
+            toggleSidebar={toggleSidebar}
+            isCollapseSidebar={isCollapseSidebar}
+          />
+          {children}
+        </div>
+      </Flex>
+      {isPending && (
+        <LoadingIndicator width={10} height={10} isFullWidth={true} />
+      )}
+    </>
   );
 }

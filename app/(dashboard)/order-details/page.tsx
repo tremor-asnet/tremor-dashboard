64d64c173df18e1bdba1579b226df0e464603feb
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2024-01-22 18:15:35 +0700

    feat(343): hook up api order detail

--- app/(dashboard)/order-details/page.tsx
+++ app/(dashboard)/order-details/page.tsx
@@ -7,21 +7,39 @@ import PaymentDetails from "@/components/OrderDetails/PaymentDetails";
 import BillingInfo from "@/components/OrderDetails/BillingInfo";
 
 // Mocks
-import {
-  mockBillingInfo,
-  mockInvoiceHeader,
-  mockOrderSummary,
-  mockTrackOrder,
-} from "@/mocks/orderDetails";
 import { ORDER_DATA } from "@/mocks";
 
-const OrderDetailsPage = () => {
+// Services
+import { getOrderDetails } from "@/services/ordersServices";
+
+const OrderDetailsPage = async () => {
+  const orderDetails = await getOrderDetails();
+  const {
+    id,
+    orderCode,
+    orderDeliverPrice,
+    orderTax,
+    trackOrderInfo,
+    billingInfo,
+    createdAt,
+  } = orderDetails;
+
+  const monies = {
+    productPrice: 123,
+    delivery: orderDeliverPrice,
+    taxes: orderTax,
+  };
+
   return (
     <Flex justifyContent="center" className="w-full bg-transparent">
       <Card className="w-full md:w-2/3 dark:bg-dark_blue">
         <Grid numItems={1} numItemsMd={2} numItemsLg={3} className="gap-2">
           <Col numColSpan={1} numColSpanMd={2} numColSpanLg={3}>
-            <InvoiceHeader {...mockInvoiceHeader} />
+            <InvoiceHeader
+              id={id}
+              createdAt={createdAt}
+              orderCode={orderCode}
+            />
             <Divider />
           </Col>
           <Col numColSpan={1} numColSpanMd={2} numColSpanLg={3}>
@@ -31,29 +49,18 @@ const OrderDetailsPage = () => {
           <Col numColSpan={1} numColSpanMd={2} numColSpanLg={3}>
             <Grid numItems={1} numItemsMd={2} numItemsLg={3} className="gap-4">
               <div className="w-full">
-                <TrackOrder
-                  id={mockTrackOrder.id}
-                  generateOrderAt={mockTrackOrder.generateOrderId}
-                  deliveredAt={mockTrackOrder.deliveredAt}
-                  transmitedToCourierAt={mockTrackOrder.transmitedToCourierAt}
-                  generateOrderId={mockTrackOrder.generateOrderId}
-                />
+                <TrackOrder {...trackOrderInfo} />
               </div>
               <div className="w-full">
                 <div className="mb-4">
                   <PaymentDetails />
                 </div>
 
-                <BillingInfo
-                  ownerName={mockBillingInfo.ownerName}
-                  companyName={mockBillingInfo.companyName}
-                  email={mockBillingInfo.email}
-                  vat={mockBillingInfo.vat}
-                />
+                <BillingInfo {...billingInfo} />
               </div>
               <Col numColSpanMd={2} numColSpanLg={1}>
                 <div className="w-full lg:pl-20">
-                  <OrderSummary {...mockOrderSummary} />
+                  <OrderSummary {...monies} />
                 </div>
               </Col>
             </Grid>
--- app/services/ordersServices.ts
+++ app/services/ordersServices.ts
@@ -20,3 +20,20 @@ export const getOrders = async () => {
 
   return res.json();
 };
+
+export const getOrderDetails = async () => {
+  const res = await fetch(`${ROUTER_API_URL}/orders/detail`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+    next: {
+      // Re-validate every minute
+      revalidate: 60,
+    },
+  });
+
+  if (!res.ok) throw new Error(getErrorMessage(res.status, res.statusText));
+
+  return res.json();
+};

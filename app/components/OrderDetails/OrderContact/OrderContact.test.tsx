2957dbbbbce7ac5266b2e455842925756236d2d6
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2024-01-23 18:27:25 +0700

    feat(343): update

--- app/components/OrderDetails/OrderContact/OrderContact.test.tsx
+++ app/components/OrderDetails/OrderContact/OrderContact.test.tsx
@@ -7,6 +7,7 @@ describe("Testing Order detail contact section", () => {
   const propsDefault = {
     name: "Leather Wallet",
     url: "https://demos.creative-tim.com/nextjs-material-dashboard-pro//_next/static/media/black-chair.b2719b4f.jpeg",
+    date: "2024-01-11T09:48:52Z",
   };
 
   it("Should match snapshot", () => {

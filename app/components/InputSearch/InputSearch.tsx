09c65921edca85482900be67a219eb2b4746387a
duy.ngo <duy.ngo@asnet.com.vn>
2024-01-23 11:15:41 +0700

    feat(323): rename component

--- app/components/InputSearch/InputSearch.tsx
+++ app/components/InputSearch/InputSearch.tsx
@@ -1,18 +1,18 @@
 import { Flex, TextInput } from "@tremor/react";
 
 interface InputSearchProps {
-  onSearch: () => void;
+  onChange: () => void;
 }
 
-const OrderSearch = ({ onSearch }: InputSearchProps) => (
+const InputSearch = ({ onChange }: InputSearchProps) => (
   <Flex className="p-6 items-start justify-end">
     <TextInput
       id="search_order"
       className="w-auto"
-      onChange={onSearch}
+      onChange={onChange}
       placeholder="Search..."
     />
   </Flex>
 );
 
-export default OrderSearch;
+export default InputSearch;

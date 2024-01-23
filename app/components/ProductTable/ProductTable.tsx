ff0c9b04589128bf6287b2337721ba7d90bd5a0b
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-01-23 10:09:42 +0700

    feat(351): update style text product table for order list and product list

--- app/components/ProductTable/ProductTable.tsx
+++ app/components/ProductTable/ProductTable.tsx
@@ -39,27 +39,29 @@ const ProductTable = ({ data }: ProductTableProps): JSX.Element => {
   };
 
   return (
-    <Card className="p-0 border-none ring-0 dark:bg-dark-tremor-primary overflow-x-auto">
-      <Flex flexDirection="col" className="items-start justify-start my-2">
+    <Card className="p-0 border-none ring-0 dark:text-light dark:bg-dark-tremor-primary overflow-x-auto">
+      <Flex
+        flexDirection="col"
+        className="items-start justify-start my-2 dark:bg-dark-tremor-primary">
         <Table className="w-full">
           <TableHead>
             <TableRow className="border-0 border-b border-gray-100">
-              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] tracking-[0.2px] font-bold opacity-70 uppercase">
+              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] dark:text-white tracking-[0.2px] font-bold opacity-70 uppercase">
                 <HeaderCellContents title="id" />
               </TableHeaderCell>
-              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] tracking-[0.2px] font-bold opacity-70 uppercase">
+              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] dark:text-white tracking-[0.2px] font-bold opacity-70 uppercase">
                 <HeaderCellContents title="date" />
               </TableHeaderCell>
-              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] tracking-[0.2px] font-bold opacity-70 uppercase">
+              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] dark:text-white tracking-[0.2px] font-bold opacity-70 uppercase">
                 <HeaderCellContents title="status" />
               </TableHeaderCell>
-              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] tracking-[0.2px] font-bold opacity-70 uppercase">
+              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] dark:text-white tracking-[0.2px] font-bold opacity-70 uppercase">
                 <HeaderCellContents title="customer" />
               </TableHeaderCell>
-              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] tracking-[0.2px] font-bold opacity-70 uppercase">
+              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] dark:text-white tracking-[0.2px] font-bold opacity-70 uppercase">
                 <HeaderCellContents title="product" />
               </TableHeaderCell>
-              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] tracking-[0.2px] font-bold opacity-70 uppercase">
+              <TableHeaderCell className="px-6 py-2 text-[10.4px] leading-[17px] dark:text-white tracking-[0.2px] font-bold opacity-70 uppercase">
                 <HeaderCellContents title="revenue" />
               </TableHeaderCell>
             </TableRow>
@@ -76,20 +78,20 @@ const ProductTable = ({ data }: ProductTableProps): JSX.Element => {
                         handleCheckBox={handleCheckBox}
                         checked={false}
                       />
-                      <Text className="ml-4 text-xs font-semibold leading-[15px] tracking-[0.4px] order-id">
+                      <Text className="ml-4 dark:text-white dark:opacity-70 text-xs font-semibold leading-[15px] tracking-[0.4px] order-id">
                         #{id}
                       </Text>
                     </Flex>
                   </TableCell>
                   <TableCell className="px-6 py-5 border-0 border-b border-gray-100">
-                    <Text className="text-xs font-semibold leading-[15px] tracking-[0.4px] order-dagte">
+                    <Text className="text-xs dark:text-white dark:opacity-70 font-semibold leading-[15px] tracking-[0.4px] order-dagte">
                       {createdAt}
                     </Text>
                   </TableCell>
                   <TableCell className="px-6 py-5 border-0 border-b border-gray-100">
                     <Flex className="justify-start text-xs font-semibold leading-[15px] tracking-[0.4px] capitalize order-status">
                       {ProductStatus(status)}
-                      <Text className="text-xs font-semibold leading-[15px] tracking-[0.4px] capitalize order-status">
+                      <Text className="text-xs dark:text-white dark:opacity-70 font-semibold leading-[15px] tracking-[0.4px] capitalize order-status">
                         {STATUS_TEXT[status]}
                       </Text>
                     </Flex>
@@ -110,7 +112,7 @@ const ProductTable = ({ data }: ProductTableProps): JSX.Element => {
                           {customer.full_name.substring(0, 1)}
                         </Flex>
                       )}
-                      <Text className="text-xs font-semibold leading-[15px] tracking-[0.4px] capitalize order-customer">
+                      <Text className="text-xs dark:text-white dark:opacity-70 font-semibold leading-[15px] tracking-[0.4px] capitalize order-customer">
                         {customer.full_name}
                       </Text>
                     </Flex>
@@ -119,13 +121,13 @@ const ProductTable = ({ data }: ProductTableProps): JSX.Element => {
                     {products?.map((product: ProductOrder, index: number) => (
                       <Text
                         key={`Product ${index} of ${product.name} by ${product.id}`}
-                        className="text-xs font-semibold leading-[15px] tracking-[0.4px] truncate max-w-[100px] lg:max-w-[200px] xl:max-w-[300px] 2xl:max-w-[400px] min-w-[100px] order-product">
+                        className="text-xs dark:text-white dark:opacity-70 font-semibold leading-[15px] tracking-[0.4px] truncate max-w-[100px] lg:max-w-[200px] xl:max-w-[300px] 2xl:max-w-[400px] min-w-[100px] order-product">
                         {product.name}
                       </Text>
                     ))}
                   </TableCell>
                   <TableCell className="px-6 py-5 border-0 border-b border-gray-100">
-                    <Text className="text-xs font-semibold leading-[15px] tracking-[0.4px] order-revenue">
+                    <Text className="text-xs dark:text-white dark:opacity-70 font-semibold leading-[15px] tracking-[0.4px] order-revenue">
                       {revenue}
                     </Text>
                   </TableCell>

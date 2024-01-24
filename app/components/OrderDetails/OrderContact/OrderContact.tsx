6071a27d8a4dee88a222e4cd9148125fcb8a54e3
duy.ngo <duy.ngo@asnet.com.vn>
2024-01-24 13:56:14 +0700

    fix(363): remove a comment

--- app/components/OrderDetails/OrderContact/OrderContact.tsx
+++ app/components/OrderDetails/OrderContact/OrderContact.tsx
@@ -46,7 +46,6 @@ const OrderContact = ({
               contact us
             </Text>
           </Button>
-          {/* <Text className="mt-3 text-sm dark:text-dark-romance font-light opacity-100 text-secondary text-right leading-[21px] tracking-[0.4px] max-w-[100px] lg:max-w-[200px] xl:max-w-[300px] 2xl:max-w-[400px] min-w-[100px]"> */}
           <Text className="mt-3 text-sm dark:text-dark-romance font-light opacity-100 text-secondary text-right leading-[21px] w-full">
             Do you like the item? Leave us a review{" "}
             <Link href="#" className="font-semibold">

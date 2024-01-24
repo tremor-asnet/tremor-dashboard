8c65abd7b4958c6859d316d3dc6a45e4d743e882
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-01-24 12:22:35 +0700

    fix(316): remove last item in list sale by country

--- app/components/SalesByCountry/SalesByCountry.tsx
+++ app/components/SalesByCountry/SalesByCountry.tsx
@@ -17,6 +17,7 @@ import { CURRENCY, FLAG_SRC } from "@/constants";
 
 // Helpers
 import { formattedNumber } from "@/helpers";
+import SalesByCountryRow from "../SalesByCountryRow";
 
 export interface ISalesByCountry {
   id: string;
@@ -64,70 +65,23 @@ const SalesByCountry = ({
         <Flex className="items-start justify-start border-0 border-b border-gray-100 last:border-transparent">
           <Table className="w-full">
             <TableBody className="last-child:border-black">
-              {data.map(item => {
-                const { id, country, sales, value, bounce } = item;
-
-                return (
-                  <TableRow key={id} className="border-solid">
-                    <TableCell className="px-4 py-3 w-6/12 sm:w-[30%] border-gray-500">
-                      <Flex className="items-center">
-                        <Image
-                          src={FLAG_SRC[id]}
-                          width="24"
-                          height="18"
-                          alt={item.country}
-                          priority
-                        />
-                        <Flex className="items-start flex-col ml-6">
-                          <Text className="text-xs dark:text-dark-romance font-semibold leading-[1.25] tracking-[0.03333em]">
-                            Country:
-                          </Text>
-                          <Text className="text-tremor-content-title dark:text-dark-tremor-content-title leading-[1.5]">
-                            {country}
-                          </Text>
-                        </Flex>
-                      </Flex>
-                    </TableCell>
-                    <TableCell className="px-4 py-3 text-left">
-                      <Text className="text-left text-xs dark:text-dark-romance font-semibold leading-[1.25]">
-                        Sales:
-                      </Text>
-                      <Text className="text-left text-tremor-content-title dark:text-dark-tremor-content-title leading-[1.5]">
-                        {formattedNumber({
-                          value: sales,
-                          isDecimalNumber: true,
-                        })}
-                      </Text>
-                    </TableCell>
-                    {isAnalytics && (
-                      <TableCell className="px-4 py-3 text-left value">
-                        <Text className="text-xs dark:text-dark-romance font-semibold leading-[1.25]">
-                          Value:
-                        </Text>
-                        <Text className="text-left text-tremor-content-title dark:text-dark-tremor-content-title leading-[1.5] truncate max-w-[60px] xs:max-w-[70px] xl:max-w-[105px]">
-                          <span>
-                            {formattedNumber({
-                              value,
-                              currency: CURRENCY.DOLLAR,
-                            })}
-                          </span>
-                        </Text>
-                      </TableCell>
-                    )}
-                    <TableCell
-                      className={`px-4 py-3 text-left bounce ${
-                        isAnalytics && "hidden md:table-cell"
-                      }`}>
-                      <Text className="text-left text-xs dark:text-dark-romance font-semibold leading-[1.25]">
-                        Bounce:
-                      </Text>
-                      <Text className="text-left text-tremor-content-title dark:text-dark-tremor-content-title leading-[1.5]">
-                        <span>{bounce}%</span>
-                      </Text>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
+              {isAnalytics
+                ? data.slice(0, -1).map(item => (
+                    <>
+                      <SalesByCountryRow
+                        data={item}
+                        isAnalytics={isAnalytics}
+                      />
+                    </>
+                  ))
+                : data.map(item => (
+                    <>
+                      <SalesByCountryRow
+                        data={item}
+                        isAnalytics={isAnalytics}
+                      />
+                    </>
+                  ))}
             </TableBody>
           </Table>
         </Flex>

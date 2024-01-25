9946c31015cf0c6335b6d9e90fe1d8b01639b9f7
Thao Pham Duy <thao.phamduy@asnet.com.vn>
2024-01-25 18:29:36 +0700

    feat(378): implement UI, stories and unit test for media section

---
+++ app/components/NewProduct/Media/Media.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+// Components
+import Media from "./Media";
+
+const propsDefault = {
+  name: "Gold Glasses",
+  url: "https://demos.creative-tim.com/nextjs-material-dashboard-pro//_next/static/media/product-12.0b55635d.jpg",
+};
+
+const meta = {
+  title: "Components/Media",
+  component: Media,
+  tags: ["autodocs"],
+} as Meta<typeof Media>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  render: () => <Media {...propsDefault} />,
+};
---
+++ app/components/NewProduct/Media/Media.test.tsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+
+// Components
+import Media from "./Media";
+
+describe("Testing Order detail contact section", () => {
+  const propsDefault = {
+    name: "Gold Glasses",
+    url: "https://demos.creative-tim.com/nextjs-material-dashboard-pro//_next/static/media/product-12.0b55635d.jpg",
+  };
+
+  it("Should match snapshot", () => {
+    const component = render(<Media {...propsDefault} />);
+    expect(component).toMatchSnapshot();
+  });
+});

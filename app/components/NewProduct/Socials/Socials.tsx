647cbf139e909f36d98afb9e6d0717e5a22186d6
Thao Pham Duy <thao.phamduy@asnet.com.vn>
2024-01-26 16:23:02 +0700

    feat(379): update dark theme for socials section

--- app/components/NewProduct/Socials/Socials.tsx
+++ app/components/NewProduct/Socials/Socials.tsx
@@ -2,13 +2,13 @@
 import { Title, Flex } from "@tremor/react";
 
 const Socials = () => {
-  const inputClasses = `w-full text-sm text-tremor-content-title py-1 mb-6 rounded-none ring-0 border-0 border-b focus:border-b-2 border-gray-300 focus:border-tremor-brand-subtle focus:outline-none hover:bg-transparent dark:bg-transparent dark:hover:bg-transparent dark:border-white`;
+  const inputClasses = `w-full text-sm text-secondary dark:text-dark-romance text-tremor-content-title py-1 mb-6 rounded-none ring-0 border-0 border-b focus:border-b-2 border-gray-300 focus:border-tremor-brand-subtle focus:outline-none hover:bg-transparent dark:bg-transparent dark:hover:bg-transparent dark:border-white dark:text-white`;
   return (
     <>
       <Title className="font-primary font-bold text-primary dark:text-dark-primary text-xl leading-snug capitalize">
         Socials
       </Title>
-      <Flex className="flex-col items-start justify-start mt-8 text-secondary dark:text-dark-romance text-sm font-normal">
+      <Flex className="flex-col items-start justify-start mt-8">
         <input
           className={inputClasses}
           id="shoppifyHandle"

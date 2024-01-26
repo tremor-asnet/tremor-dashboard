db42cc441edfbcded814c6bd7ff4761e714f621a
Khue Nguyen <khue.nguyen@asnet.com.vn>
2024-01-26 14:59:18 +0700

    fix: update table product story

--- app/components/Table/TableProduct/TableProduct.stories.tsx
+++ app/components/Table/TableProduct/TableProduct.stories.tsx
@@ -3,8 +3,8 @@ import type { Meta, StoryObj } from "@storybook/react";
 // Components
 import TableProduct from "./TableProduct";
 
-// Types
-import { Product } from "@/types";
+// Mocks
+import { MOCK_PRODUCTS } from "@/mocks";
 
 const meta = {
   title: "Components/Tables/TableProduct",
@@ -12,64 +12,10 @@ const meta = {
   tags: ["autodocs"],
 } as Meta<typeof TableProduct>;
 
-// Mocks
-const mockProducts: Product[] = [
-  {
-    id: 230019,
-    createdAt: "2023-11-22T04:01:46+00:00",
-    productName: "Christopher Knight Home",
-    price: 89.53,
-    isAvailable: true,
-    providerName: "Provider A",
-    image:
-      "https://demos.creative-tim.com/nextjs-material-dashboard-pro//_next/static/media/black-chair.b2719b4f.jpeg",
-  },
-  {
-    id: 87120,
-    createdAt: "2023-01-09T02:00:00+00:00",
-    productName: "Bar Height Swivel Barstool",
-    price: 99.99,
-    isAvailable: true,
-    providerName: "Provider B",
-    image:
-      "https://demos.creative-tim.com/nextjs-material-dashboard-pro//_next/static/media/chair-pink.686a7ae1.jpeg",
-  },
-  {
-    id: 2456772,
-    createdAt: "2023-11-01T03:20:00+00:00",
-    productName: "Christopher Knight Home",
-    price: 89.53,
-    isAvailable: true,
-    providerName: "Provider A",
-    image:
-      "https://demos.creative-tim.com/nextjs-material-dashboard-pro//_next/static/media/black-chair.b2719b4f.jpeg",
-  },
-  {
-    id: 1992,
-    createdAt: "2023-08-08T23:55:34+00:00",
-    productName: "Modern Square",
-    price: 59.99,
-    isAvailable: false,
-    providerName: "Provider C",
-    image:
-      "https://demos.creative-tim.com/nextjs-material-dashboard-pro//_next/static/media/chair-wood.d127f22b.jpeg",
-  },
-  {
-    id: 412301,
-    createdAt: "2023-02-19T02:08:08+00:00",
-    productName: "Signature Design by Ashley",
-    price: 129,
-    isAvailable: true,
-    providerName: "Provider B",
-    image:
-      "https://demos.creative-tim.com/nextjs-material-dashboard-pro//_next/static/media/chair-steel.dd1d3892.jpeg",
-  },
-];
-
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
-  render: () => <TableProduct products={mockProducts} />,
+  render: () => <TableProduct products={MOCK_PRODUCTS} />,
 };

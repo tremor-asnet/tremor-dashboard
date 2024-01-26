4394732561ca63ef7682eb9fabb83586c6bf59fb
Khue Nguyen <khue.nguyen@asnet.com.vn>
2024-01-26 14:09:54 +0700

    fix: update common table component

--- app/components/Table/common/CustomCheckBoxField/index.tsx
+++ app/components/Table/common/CustomCheckBoxField/index.tsx
@@ -2,19 +2,21 @@
 import { Checkbox } from "@/components";
 import Link from "next/link";
 
-// Constants
-import { ROUTES } from "@/constants";
-
 interface CustomCheckboxFieldProps {
   id: number;
+  link: string;
   onChange: () => void;
 }
 
-const CustomCheckBoxField = ({ id, onChange }: CustomCheckboxFieldProps) => (
+const CustomCheckBoxField = ({
+  id,
+  link,
+  onChange,
+}: CustomCheckboxFieldProps) => (
   <div className="flex justify-start items-center ml-2">
     <Checkbox onChange={onChange} />
     <Link
-      href={`${ROUTES.ORDER_LIST}/${id}`}
+      href={link}
       className="ml-4 text-xs dark:text-white font-semibold leading-[15px] tracking-[0.4px] order-id hover:underline">
       &#35;{id}
     </Link>

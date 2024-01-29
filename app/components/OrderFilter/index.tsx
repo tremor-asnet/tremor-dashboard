73fedf451f9954f268b0846eaefcd04c643d9f1d
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-01-29 11:02:51 +0700

    feat(348): remove ul tag

--- app/components/OrderFilter/index.tsx
+++ app/components/OrderFilter/index.tsx
@@ -76,18 +76,16 @@ const OrderFilter = ({ title }: OrderFilterProps) => {
       {showListOption && (
         <div ref={selectRef as RefObject<HTMLDivElement>}>
           <ul className="absolute z-[1] w-[160px] right-0 shadow-tremor-cardImage dark:shadow-dark-select-option bg-secondary p-2 rounded-md dark:bg-dark-tremor-primary">
-            <ul>
-              {orderListOption.map(({ option, value }) => (
-                <FilterItem
-                  key={option}
-                  title="Status"
-                  option={option}
-                  value={value}
-                  additionalClass="w-full text-tremor-default cursor-pointer text-secondary px-4 py-[0.3rem] hover:bg-body hover:text-tremor-brand-subtle hover:rounded-md min-h-[auto] dark:text-dark-romance dark:hover:bg-dark-secondary"
-                  onSelectFilter={handleSelectFilter}
-                />
-              ))}
-            </ul>
+            {orderListOption.map(({ option, value }) => (
+              <FilterItem
+                key={option}
+                title="Status"
+                option={option}
+                value={value}
+                additionalClass="w-full text-tremor-default cursor-pointer text-secondary px-4 py-[0.3rem] hover:bg-body hover:text-tremor-brand-subtle hover:rounded-md min-h-[auto] dark:text-dark-romance dark:hover:bg-dark-secondary"
+                onSelectFilter={handleSelectFilter}
+              />
+            ))}
             <div className="h-px bg-gradient-select my-2 opacity-25 dark:bg-gradient-divider" />
             <li
               className="w-full text-tremor-default cursor-pointer text-attention px-4 py-[0.3rem] hover:bg-body hover:rounded-md min-h-[auto] dark:hover:bg-dark-secondary"

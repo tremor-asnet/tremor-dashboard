c6a247225d74bba364c5917e131d8ecc014318cc
Khue Nguyen <khue.nguyen@asnet.com.vn>
2024-01-29 15:14:58 +0700

    fix: cache variables for re-use

--- app/components/common/Stepper/Stepper.tsx
+++ app/components/common/Stepper/Stepper.tsx
@@ -4,9 +4,11 @@ interface StepperProps {
 }
 
 const Stepper = ({ currentStep, steps }: StepperProps) => {
+  const stepsLength = steps.length;
+
   return (
     <ul
-      className={`grid grid-cols-${steps.length} pt-6 pb-4 bg-zinc-700 dark:stepper-dark-bg rounded-lg`}>
+      className={`grid grid-cols-${stepsLength} pt-6 pb-4 bg-zinc-700 dark:stepper-dark-bg rounded-lg`}>
       {/* First Items */}
       <li>
         <div className="flex items-center w-full h-4 pl-[50%]">
@@ -19,56 +21,60 @@ const Stepper = ({ currentStep, steps }: StepperProps) => {
         <p className="stepper-text-content text-white mt-2">1. {steps[0]}</p>
       </li>
       {/* Body items */}
-      {steps.length > 2
-        ? steps.slice(1, steps.length - 1).map((step, idx) => (
-            <li key={step}>
-              <div className="flex items-center w-full h-4">
-                {/* Left line */}
-                <span
-                  className={`h-0.5 w-[50%] ${
-                    currentStep >= idx + 2 ? "bg-white" : "bg-zinc-400"
-                  }`}></span>
-                {/* Dot */}
-                <span
-                  className={`${
-                    currentStep >= idx + 2
-                      ? "stepper-dot-active"
-                      : "stepper-dot"
-                  } rounded-full`}></span>
-                {/* Right line */}
-                <span
-                  className={`h-0.5 w-[50%] ${
-                    currentStep > idx + 2 ? "bg-white" : "bg-zinc-400"
-                  }`}></span>
-              </div>
-              <p
-                className={`stepper-text-content mt-2 ${
-                  currentStep >= idx + 2 ? "text-white" : "text-zinc-400"
-                }`}>
-                {idx + 2}. {steps[idx + 1]}
-              </p>
-            </li>
-          ))
+      {stepsLength > 2
+        ? steps.slice(1, stepsLength - 1).map((step, idx) => {
+            const itemStepIndex = idx + 2;
+
+            return (
+              <li key={step}>
+                <div className="flex items-center w-full h-4">
+                  {/* Left line */}
+                  <span
+                    className={`h-0.5 w-[50%] ${
+                      currentStep >= itemStepIndex ? "bg-white" : "bg-zinc-400"
+                    }`}></span>
+                  {/* Dot */}
+                  <span
+                    className={`${
+                      currentStep >= itemStepIndex
+                        ? "stepper-dot-active"
+                        : "stepper-dot"
+                    } rounded-full`}></span>
+                  {/* Right line */}
+                  <span
+                    className={`h-0.5 w-[50%] ${
+                      currentStep > itemStepIndex ? "bg-white" : "bg-zinc-400"
+                    }`}></span>
+                </div>
+                <p
+                  className={`stepper-text-content mt-2 ${
+                    currentStep >= itemStepIndex
+                      ? "text-white"
+                      : "text-zinc-400"
+                  }`}>
+                  {itemStepIndex}. {steps[idx + 1]}
+                </p>
+              </li>
+            );
+          })
         : null}
       {/* Last Item */}
       <li>
         <div className="flex items-center w-full h-4">
           <span
             className={`h-0.5 w-[50%] ${
-              currentStep === steps.length ? "bg-white" : "bg-zinc-400"
+              currentStep === stepsLength ? "bg-white" : "bg-zinc-400"
             }`}></span>
           <span
             className={`rounded-full ${
-              currentStep === steps.length
-                ? "stepper-dot-active"
-                : "stepper-dot"
+              currentStep === stepsLength ? "stepper-dot-active" : "stepper-dot"
             }`}></span>
         </div>
         <p
           className={`stepper-text-content mt-2 ${
             currentStep === steps.length ? "text-white" : "text-zinc-400"
           }`}>
-          {steps.length}. {steps[steps.length - 1]}
+          {stepsLength}. {steps[stepsLength - 1]}
         </p>
       </li>
     </ul>

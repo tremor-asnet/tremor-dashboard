f88856889f6ddf125ebf8ad390b119e543d35108
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2024-01-31 13:48:57 +0700

    feat(388): update

--- app/components/Forms/EditProductForm.tsx
+++ app/components/Forms/EditProductForm.tsx
@@ -20,7 +20,6 @@ const PricingInfo = dynamic(
 
 // Services
 import { editProduct } from "@/services";
-import { useState } from "react";
 
 // Types
 import { EditProductData } from "@/types";
@@ -66,11 +65,9 @@ const EditProductForm = ({
     },
   });
 
-  const [isLoading, setIsLoading] = useState(false);
   const { handleSubmit } = formHandler;
 
   const onSubmit = async (data: EditProductData) => {
-    setIsLoading(true);
     const convertedTagsValue = data.tags.map(value => {
       return Number(value);
     });
@@ -83,45 +80,37 @@ const EditProductForm = ({
     };
 
     await editProduct(id, newData);
-    setIsLoading(false);
   };
 
   return (
-    <>
-      <FormProvider {...formHandler}>
-        <form onSubmit={handleSubmit(onSubmit)} className="relative">
-          <div className="w-full text-end absolute -mt-24">
-            <Button
-              type="submit"
-              className="antialiased text-center uppercase px-6 py-2.5 bg-gradient-primary dark:bg-gradient-pickled !shadow-btn-primary rounded-lg border-0 hover:!shadow-btn-primary-hover items-end"
-              size="xs"
-              disabled={isLoading}>
-              <Text className="items-center uppercase py-[2px] text-xs font-bold font-primary text-white dark:text-dark-tremor-content-title">
-                Save
-              </Text>
-            </Button>
+    <FormProvider {...formHandler}>
+      <form onSubmit={handleSubmit(onSubmit)} className="relative">
+        <div className="w-full text-end absolute -mt-24">
+          <Button
+            type="submit"
+            className="antialiased text-center uppercase px-6 py-2.5 bg-gradient-primary dark:bg-gradient-pickled !shadow-btn-primary rounded-lg border-0 hover:!shadow-btn-primary-hover items-end"
+            size="xs">
+            <Text className="items-center uppercase py-[2px] text-xs font-bold font-primary text-white dark:text-dark-tremor-content-title">
+              Save
+            </Text>
+          </Button>
+        </div>
+        <Grid numItemsSm={1} numItemsLg={3} className="gap-6">
+          <div className="w-full">
+            <ProductImage name={productName} desc={description} image={image} />
           </div>
-          <Grid numItemsSm={1} numItemsLg={3} className="gap-6">
-            <div className="w-full">
-              <ProductImage
-                name={productName}
-                desc={description}
-                image={image}
-              />
-            </div>
-            <Col numColSpanSm={1} numColSpanLg={2}>
-              <ProductInfo />
-            </Col>
-            <Col numColSpan={1}>
-              <Socials />
-            </Col>
-            <Col numColSpanSm={1} numColSpanLg={2}>
-              <PricingInfo />
-            </Col>
-          </Grid>
-        </form>
-      </FormProvider>
-    </>
+          <Col numColSpanSm={1} numColSpanLg={2}>
+            <ProductInfo />
+          </Col>
+          <Col numColSpan={1}>
+            <Socials />
+          </Col>
+          <Col numColSpanSm={1} numColSpanLg={2}>
+            <PricingInfo />
+          </Col>
+        </Grid>
+      </form>
+    </FormProvider>
   );
 };
 

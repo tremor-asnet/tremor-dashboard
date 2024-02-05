6e200218b66f5cafe174e204266f1a6ea42b66fe
Khue Nguyen <khue.nguyen@asnet.com.vn>
2024-02-05 16:05:09 +0700

    fix: format number

--- app/helpers/billing.tsx
+++ app/helpers/billing.tsx
@@ -2,11 +2,15 @@
 import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 import { BsExclamationLg } from "react-icons/bs";
 
+// Helpers
+import { formattedNumber } from "@/helpers";
+
 export const getContentByProps = (
   status: number,
   type: number,
   amount: number,
 ) => {
+  const formattedAmmount = formattedNumber({ value: amount });
   if (status === 0)
     return {
       color: "#5d6c85",
@@ -19,12 +23,12 @@ export const getContentByProps = (
         color: "#4CAF50",
         classes: "text-[#4CAF50] border-[#4CAF50]",
         icon: <BiChevronUp />,
-        value: `+ $ ${amount}`,
+        value: `+ $ ${formattedAmmount}`,
       }
     : {
         color: "#F44335",
         classes: "text-[#F44335] border-[#F44335]",
         icon: <BiChevronDown />,
-        value: `- $ ${amount}`,
+        value: `- $ ${formattedAmmount}`,
       };
 };

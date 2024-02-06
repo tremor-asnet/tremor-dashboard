ecdb255362897d47f0dde5ecb031da03f06b417b
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-02-07 05:55:20 +0700

    feat(479): update content data mock for salary billing

--- app/mocks/billing.tsx
+++ app/mocks/billing.tsx
@@ -3,11 +3,11 @@ import { AGGREGATION_TYPE } from "@/constants";
 
 export const mockSalaryData = [
   {
-    type: AGGREGATION_TYPE.PAYPAL,
-    value: 45500,
-  },
-  {
     type: AGGREGATION_TYPE.SALARY,
     value: 2000,
+  },
+  {
+    type: AGGREGATION_TYPE.PAYPAL,
+    value: 45500,
   },
 ];

d84508a3d7720e8c217204d31fe7879b03fc30ee
duy.ngo <duy.ngo@asnet.com.vn>
2024-02-06 15:14:13 +0700

    feat(458): udpate name function

--- app/components/common/DataGrid/DataGridHeader/DataGridHeader.tsx
+++ app/components/common/DataGrid/DataGridHeader/DataGridHeader.tsx
@@ -18,24 +18,32 @@ const DataGridHeader = <T,>({
   onHeaderClick,
   sortItem,
   hasSort,
-}: DataTableHeaderProps<T>) => (
-  <TableHead>
-    <TableRow>
-      {columns.map(column => (
-        <TableHeaderCell
-          onClick={() => onHeaderClick?.(column)}
-          key={column.key}
-          className="px-6 py-2 text-[10.4px] leading-[17px] dark:text-white tracking-[0.2px] font-bold opacity-70 uppercase cursor-pointer">
-          <HeaderCellContents
-            title={column.title}
-            keyColumn={column.key}
-            sortItem={sortItem}
-            hasSort={hasSort}
-          />
-        </TableHeaderCell>
-      ))}
-    </TableRow>
-  </TableHead>
-);
+}: DataTableHeaderProps<T>) => {
+  return (
+    <TableHead>
+      <TableRow>
+        {columns.map(column => {
+          const handleHeaderClick = () => {
+            onHeaderClick?.(column);
+          };
+
+          return (
+            <TableHeaderCell
+              onClick={handleHeaderClick}
+              key={column.key}
+              className="px-6 py-2 text-[10.4px] leading-[17px] dark:text-white tracking-[0.2px] font-bold opacity-70 uppercase cursor-pointer">
+              <HeaderCellContents
+                title={column.title}
+                keyColumn={column.key}
+                sortItem={sortItem}
+                hasSort={hasSort}
+              />
+            </TableHeaderCell>
+          );
+        })}
+      </TableRow>
+    </TableHead>
+  );
+};
 
 export default DataGridHeader;

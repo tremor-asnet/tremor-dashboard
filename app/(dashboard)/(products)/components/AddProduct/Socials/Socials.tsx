58d199cf24067cb689edc2e70640d085e63f3cd4
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2024-02-07 15:29:55 +0700

    feat(494): update validate

--- app/(dashboard)/(products)/components/AddProduct/Socials/Socials.tsx
+++ app/(dashboard)/(products)/components/AddProduct/Socials/Socials.tsx
@@ -1,54 +1,103 @@
 // Libs
-import { Controller } from "react-hook-form";
+import { Controller, FieldErrors } from "react-hook-form";
 
 // Components
 import InputField from "@/components/common/CustomField/InputField/InputField";
 
+// Constants
+import { MESSAGES_ERROR, REGEX } from "@/constants";
+
+// Types
+import { NewSocial } from "@/types";
+
 interface SocialsProps {
   control: any;
+  errors: FieldErrors<NewSocial>;
 }
 
-const Socials = ({ control }: SocialsProps) => (
-  <div className="flex flex-col gap-6">
-    <Controller
-      name="shopifyUrl"
-      control={control}
-      render={({ field }) => (
-        <InputField
-          label="Shopify Handle"
-          placeholder=""
-          type="text"
-          {...field}
-        />
-      )}
-    />
+const Socials = ({ control, errors }: SocialsProps) => {
+  const facebookErrorMsg = errors.facebookUrl?.message;
+  const instagramErrorMsg = errors.instagramUrl?.message;
+  const shopifyErrorMsg = errors.shopifyUrl?.message;
 
-    <Controller
-      name="facebookUrl"
-      control={control}
-      render={({ field }) => (
-        <InputField
-          label="Facebook Account"
-          placeholder=""
-          type="text"
-          {...field}
-        />
-      )}
-    />
+  return (
+    <div className="flex flex-col gap-6">
+      <Controller
+        name="shopifyUrl"
+        control={control}
+        rules={{
+          required: MESSAGES_ERROR.URL_REQUIRED,
+          pattern: {
+            value: REGEX.URL,
+            message: MESSAGES_ERROR.URL_REQUIRED,
+          },
+        }}
+        render={({ field }) => (
+          <>
+            <InputField
+              label="Shopify Handle"
+              placeholder=""
+              type="text"
+              {...field}
+            />
+            <p className="text-[11px] xs:text-xs text-red-500">
+              {shopifyErrorMsg}
+            </p>
+          </>
+        )}
+      />
 
-    <Controller
-      name="instagramUrl"
-      control={control}
-      render={({ field }) => (
-        <InputField
-          label="Instagram Account"
-          placeholder=""
-          type="text"
-          {...field}
-        />
-      )}
-    />
-  </div>
-);
+      <Controller
+        name="facebookUrl"
+        control={control}
+        rules={{
+          required: MESSAGES_ERROR.URL_REQUIRED,
+          pattern: {
+            value: REGEX.URL,
+            message: MESSAGES_ERROR.URL_REQUIRED,
+          },
+        }}
+        render={({ field }) => (
+          <>
+            <InputField
+              label="Facebook Account"
+              placeholder=""
+              type="text"
+              {...field}
+            />
+            <p className="text-[11px] xs:text-xs text-red-500">
+              {facebookErrorMsg}
+            </p>
+          </>
+        )}
+      />
+
+      <Controller
+        name="instagramUrl"
+        control={control}
+        rules={{
+          required: MESSAGES_ERROR.URL_REQUIRED,
+          pattern: {
+            value: REGEX.URL,
+            message: MESSAGES_ERROR.URL_REQUIRED,
+          },
+        }}
+        render={({ field }) => (
+          <>
+            <InputField
+              label="Instagram Account"
+              placeholder=""
+              type="text"
+              {...field}
+            />
+            <p className="text-[11px] xs:text-xs text-red-500">
+              {instagramErrorMsg}
+            </p>
+          </>
+        )}
+      />
+    </div>
+  );
+};
 
 export default Socials;

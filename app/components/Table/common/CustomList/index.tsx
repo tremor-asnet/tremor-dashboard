390d529261d4d15ca0e9074609b575090a856a65
Thao Pham Duy <thao.phamduy@asnet.com.vn>
2024-02-15 16:53:40 +0700

    fix(522): update ui for product and qantity in list order

--- app/components/Table/common/CustomList/index.tsx
+++ app/components/Table/common/CustomList/index.tsx
@@ -5,7 +5,13 @@ interface CustomListProps {
 }
 
 export const CustomList = ({ products }: CustomListProps) => (
-  <p className="py-0.5 text-xs dark:text-lighter font-semibold leading-[15px] tracking-[0.4px] truncate max-w-[100px] lg:max-w-[200px] xl:max-w-[300px] 2xl:max-w-[400px] min-w-[100px] order-product">
-    {products[0].name}
-  </p>
+  <>
+    {products?.map(product => (
+      <p
+        key={product.id}
+        className="py-0.5 text-xs dark:text-lighter font-semibold leading-[15px] tracking-[0.4px] truncate max-w-[100px] lg:max-w-[200px] xl:max-w-[300px] 2xl:max-w-[400px] min-w-[100px] order-product py-5 first:pt-0 last:pb-0 border-0 dark:border-grayish border-b border-gray-100 last:border-0">
+        {product.name}
+      </p>
+    ))}
+  </>
 );
--- app/components/Table/common/CustomQuantity/index.tsx
+++ app/components/Table/common/CustomQuantity/index.tsx
@@ -5,7 +5,13 @@ interface QuantityProps {
 }
 
 export const CustomQuantity = ({ products }: QuantityProps) => (
-  <p className="py-0.5 text-xs dark:text-white font-semibold leading-[15px] tracking-[0.4px] max-w-[100px] lg:max-w-[200px] xl:max-w-[300px] 2xl:max-w-[400px] min-w-[100px] order-product">
-    {products[0].count}
-  </p>
+  <>
+    {products?.map(product => (
+      <p
+        key={product.id}
+        className="py-0.5 text-xs dark:text-white font-semibold leading-[15px] tracking-[0.4px] max-w-[100px] lg:max-w-[200px] xl:max-w-[300px] 2xl:max-w-[400px] min-w-[100px] order-product py-5 first:pt-0 last:pb-0 border-0 dark:border-grayish border-b border-gray-100 last:border-0">
+        {product.count}
+      </p>
+    ))}
+  </>
 );

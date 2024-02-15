8ce06b59fc889c446afd3bc1f97e164ed6b3fb6e
Thao Pham Duy <thao.phamduy@asnet.com.vn>
2024-02-15 15:15:23 +0700

    fix(522): order list page - product & quantity show duplicate values

--- app/components/Table/common/CustomQuantity/index.tsx
+++ app/components/Table/common/CustomQuantity/index.tsx
@@ -5,13 +5,7 @@ interface QuantityProps {
 }
 
 export const CustomQuantity = ({ products }: QuantityProps) => (
-  <>
-    {products?.map(product => (
-      <p
-        key={product.id}
-        className="py-0.5 text-xs dark:text-white font-semibold leading-[15px] tracking-[0.4px] max-w-[100px] lg:max-w-[200px] xl:max-w-[300px] 2xl:max-w-[400px] min-w-[100px] order-product">
-        {product.count}
-      </p>
-    ))}
-  </>
+  <p className="py-0.5 text-xs dark:text-white font-semibold leading-[15px] tracking-[0.4px] max-w-[100px] lg:max-w-[200px] xl:max-w-[300px] 2xl:max-w-[400px] min-w-[100px] order-product">
+    {products[0].count}
+  </p>
 );

90ef9bdefb31f054ad2af97284890c26e79e46f8
Thao Pham Duy <thao.phamduy@asnet.com.vn>
2024-02-19 15:21:06 +0700

    feat(484): import invoice header component to invoice detail page

--- app/(dashboard)/invoice/page.tsx
+++ app/(dashboard)/invoice/page.tsx
@@ -1,13 +1,18 @@
+import dynamic from "next/dynamic";
+
 // Components
 import { Card, Col, Flex, Grid } from "@tremor/react";
+const InvoiceHeader = dynamic(
+  () => import("@/components/Invoice/InvoiceHeader/InvoiceHeader"),
+);
 
 const Invoice = () => {
   return (
     <Flex justifyContent="center" className="w-full bg-transparent">
       <Card className="w-full lg:w-2/3 dark:bg-dark_blue p-4 ring-0 rounded-xl shadow-md">
         <Grid numItems={1} className="gap-2">
-          <Col numColSpan={1} className="h-48">
-            Invoice Header
+          <Col numColSpan={1}>
+            <InvoiceHeader />
           </Col>
           <Col numColSpan={1} className="h-48">
             Invoice Body

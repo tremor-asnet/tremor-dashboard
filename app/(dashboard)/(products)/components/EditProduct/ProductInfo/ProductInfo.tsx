41730f25695f6fe574779c1766451b177f855306
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2024-02-20 11:13:52 +0700

    Fix/saving behavior error

--- app/(dashboard)/(products)/components/EditProduct/ProductInfo/ProductInfo.tsx
+++ app/(dashboard)/(products)/components/EditProduct/ProductInfo/ProductInfo.tsx
@@ -24,15 +24,13 @@ import { EXCEPT_KEYS } from "@/constants/common";
 import "@/styles/form.css";
 import "react-quill/dist/quill.snow.css"; // Import Quill styles
 
+// Hooks
+import useFocusFieldError from "@/hooks/useFocusFieldError";
+
 const ProductInfo = () => {
-  const {
-    control,
-    formState: { errors },
-  } = useFormContext();
-  const { productName, weight, quantity } = errors || {};
-  const nameErrorMessage = productName?.message?.toString() || "";
-  const weightErrorMessage = weight?.message?.toString() || "";
-  const quantityErrorMessage = quantity?.message?.toString();
+  const { control, formState } = useFormContext();
+
+  useFocusFieldError(formState);
 
   const handleOnKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     EXCEPT_KEYS.POSITIVE_DOUBLE.includes(e.key) && e.preventDefault();
@@ -55,16 +53,20 @@ const ProductInfo = () => {
                 message: MESSAGES_ERROR.NAME_MIN_LENGTH,
               },
             }}
-            render={({ field }) => (
-              <div className="w-full mb-4">
-                <InputField id="edit-name" label="Name" {...field} />
-                {nameErrorMessage && (
-                  <p className="pt-1 text-[11px] xs:text-xs text-red-500">
-                    {nameErrorMessage}
-                  </p>
-                )}
-              </div>
-            )}
+            render={({ field, formState: { errors } }) => {
+              const productNameErrorMessage = errors.productName;
+
+              return (
+                <div className="w-full mb-4">
+                  <InputField
+                    id="edit-name"
+                    label="Name"
+                    errorMessage={productNameErrorMessage}
+                    {...field}
+                  />
+                </div>
+              );
+            }}
             name="productName"
           />
 
@@ -91,22 +93,22 @@ const ProductInfo = () => {
             rules={{
               pattern: { value: DECIMAL_REGEX, message: "Invalid weight" },
             }}
-            render={({ field }) => (
-              <div className="w-full mb-4">
-                <InputField
-                  id="edit-weight"
-                  type="number"
-                  label="Weight"
-                  {...field}
-                  onKeyDown={handleOnKeyDown}
-                />
-                {weightErrorMessage && (
-                  <p className="pt-1 text-[11px] xs:text-xs text-red-500">
-                    {weightErrorMessage}
-                  </p>
-                )}
-              </div>
-            )}
+            render={({ field, formState: { errors } }) => {
+              const weightErrorMessage = errors.weight;
+
+              return (
+                <div className="w-full mb-4">
+                  <InputField
+                    id="edit-weight"
+                    type="number"
+                    label="Weight"
+                    errorMessage={weightErrorMessage}
+                    onKeyDown={handleOnKeyDown}
+                    {...field}
+                  />
+                </div>
+              );
+            }}
             name="weight"
           />
 
@@ -118,22 +120,22 @@ const ProductInfo = () => {
                 message: "Invalid quantity number",
               },
             }}
-            render={({ field }) => (
-              <div className="w-full mb-4">
-                <InputField
-                  id="edit-quantity"
-                  type="number"
-                  label="Quantity"
-                  onKeyDown={handleOnKeyDown}
-                  {...field}
-                />
-                {quantityErrorMessage && (
-                  <p className="pt-1 text-[11px] xs:text-xs text-red-500">
-                    {quantityErrorMessage}
-                  </p>
-                )}
-              </div>
-            )}
+            render={({ field, formState: { errors } }) => {
+              const quantityErrorMessage = errors.quantity;
+
+              return (
+                <div className="w-full mb-4">
+                  <InputField
+                    id="edit-quantity"
+                    type="number"
+                    label="Quantity"
+                    errorMessage={quantityErrorMessage}
+                    onKeyDown={handleOnKeyDown}
+                    {...field}
+                  />
+                </div>
+              );
+            }}
             name="quantity"
           />
 

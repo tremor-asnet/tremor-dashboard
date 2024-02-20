51cae0f8cf6c25016c8f93fd49a5d65be827e550
Thao Pham Duy <thao.phamduy@asnet.com.vn>
2024-02-20 10:12:25 +0700

    feat(486): update line height and letter space for text of invoice header section

--- app/components/Invoice/InvoiceHeader/InvoiceHeader.tsx
+++ app/components/Invoice/InvoiceHeader/InvoiceHeader.tsx
@@ -12,20 +12,20 @@ export const InvoiceHeader = () => {
         alignItems="start"
         className="md:max-w-[33.333333%]">
         <InvoiceLogo width={16} height={16} />
-        <p className="text-primary dark:text-white font-bold leading-[26px] tracking-[0.12px] mt-3">
+        <p className="text-primary dark:text-white font-bold leading-6 tracking-wide mt-6">
           St. Independence Embankment, 050105 Bucharest, Romania
         </p>
-        <p className="text-secondary dark:text-dark-romance font-primary font-light leading-[25.6px] tracking-[0.17136px] mt-2 mb-4">
+        <p className="text-secondary dark:text-dark-romance font-primary font-light leading-6 tracking-wide mt-2 mb-4">
           tel: +4 (074) 1090873
         </p>
       </Flex>
       <Flex
         flexDirection="col"
         className="items-start md:items-end mt-10 md:mt-0">
-        <p className="text-primary dark:text-white font-bold leading-[26px] tracking-[0.12px]">
+        <p className="text-primary dark:text-white font-bold leading-6 tracking-wide">
           Billed to: John Doe
         </p>
-        <p className="text-secondary dark:text-dark-romance font-primary font-light leading-[25.6px] tracking-[0.17136px] md:text-right">
+        <p className="text-secondary dark:text-dark-romance font-primary font-light leading-6 tracking-wide md:text-right">
           4006 Locust View Drive
           <br /> San Francisco CA
           <br /> California

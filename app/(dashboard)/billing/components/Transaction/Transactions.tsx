bac90e4161fde44aa59559281e5858542959bae5
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-02-21 14:21:50 +0700

    feat(556): update style for transaction list darkmode text colors

--- app/(dashboard)/billing/components/Transaction/Transactions.tsx
+++ app/(dashboard)/billing/components/Transaction/Transactions.tsx
@@ -1,5 +1,5 @@
 // Icons
-import { FaCalendarAlt } from "react-icons/fa";
+import { FaRegCalendarAlt } from "react-icons/fa";
 
 // Components
 import TransactionList from "../TransactionList/TransactionList";
@@ -16,15 +16,15 @@ const Transactions = ({ newest, date }: TransactionsProps) => {
   return (
     <div className="p-6 bg-white dark:bg-[#202940] rounded-lg shadow-md">
       <div className="flex justify-between">
-        <h6 className="font-semibold text-[#344767] dark:text-white">
-          Your Transactions
+        <h6 className="font-semibold text-primary dark:text-white">
+          Your Transaction&#39;s
         </h6>
-        <div className="flex items-center text-[#7b809a]">
-          <FaCalendarAlt />
-          <p className="text-sm ml-2">{date}</p>
+        <div className="flex items-center text-secondary">
+          <FaRegCalendarAlt className="dark:text-lighter" />
+          <p className="text-sm dark:text-lighter ml-2">{date}</p>
         </div>
       </div>
-      <h6 className="transaction-list-heading">NEWEST</h6>
+      <h6 className="transaction-list-heading dark:text-lighter">NEWEST</h6>
       <div className="overflow-y-auto max-h-[505px]">
         <TransactionList transactions={newest} />
       </div>

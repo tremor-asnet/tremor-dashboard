73ec77782b049144821025a4281a316b432513e7
duy.ngo <duy.ngo@asnet.com.vn>
2024-02-23 10:49:36 +0700

    fix(528): fix no page custom font nextjs warning

--- app/ClientRootLayout.tsx
+++ app/ClientRootLayout.tsx
@@ -25,21 +25,9 @@ const ClientRootLayout = ({ children }: ClientRootLayoutProps) => {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/assets/images/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      {/* TODO: Add class to custom theme dark or light */}
-      <body className={`${inter.className} ${theme ? "dark" : "light"}`}>
-        {children}
-      </body>
-    </html>
+    <div className={`${inter.className} ${theme ? "dark" : "light"}`}>
+      {children}
+    </div>
   );
 };
 

eb5ab6f11574508998c48f5e4f83ac1ca0c35cf8
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-02-28 17:29:19 +0700

    feat(636): update prop default for LoadingIndicator component

--- app/(dashboard)/(products)/product-list/[id]/edit-product/loading.tsx
+++ app/(dashboard)/(products)/product-list/[id]/edit-product/loading.tsx
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <Flex className="grow w-full items-center justify-center">
       <Flex flexDirection="col" className="grow w-full h-full items-center">
-        <LoadingIndicator width={16} height={16} fillColor="river-bed-500" />
+        <LoadingIndicator fillColor="river-bed-500" />
         <h2 className="mt-2 text-gray-400">Loading</h2>
       </Flex>
     </Flex>

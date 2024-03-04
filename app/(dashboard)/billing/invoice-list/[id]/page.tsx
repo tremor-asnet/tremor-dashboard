51aea955d1a350e109d238a040a3bdb1c8dd1b4f
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2024-03-04 15:34:57 +0700

    feat(612): update safari print layout

--- app/(dashboard)/billing/invoice-list/[id]/page.tsx
+++ app/(dashboard)/billing/invoice-list/[id]/page.tsx
@@ -10,6 +10,9 @@ const InvoiceFooter = dynamic(
   () => import("@/ui/features/invoices/InvoiceFooter/InvoiceFooter"),
 );
 
+// Styles
+import "@/styles/billing.css";
+
 // Services
 import { getInvoiceDetails } from "@/services";
 
@@ -33,9 +36,11 @@ const InvoiceDetailsPage = async ({ params }: { params: { id: number } }) => {
   } = customer;
 
   return (
-    <Flex justifyContent="center" className="w-full bg-transparent">
+    <Flex
+      justifyContent="center"
+      className="w-full bg-transparent print-invoice">
       <Card
-        className="w-full lg:w-2/3 dark:bg-dark_blue px-6 py-7 ring-0 rounded-xl shadow-md print:shadow-none dark:print:shadow-none"
+        className="print-content w-full lg:w-2/3 dark:bg-dark_blue px-6 py-7 ring-0 rounded-xl shadow-md print:shadow-none dark:print:shadow-none"
         id="print-content">
         <Grid numItems={1} className="gap-2">
           <Col numColSpan={1}>

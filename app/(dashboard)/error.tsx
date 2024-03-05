9ef65b1aba17a7f13a48cf13a254defde39af656
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-03-05 08:17:01 +0700

    feat(668): update text color for error in dark mode

--- app/(dashboard)/error.tsx
+++ app/(dashboard)/error.tsx
@@ -2,20 +2,20 @@
 
 import { useEffect } from "react";
 
-export default function Error({
+const Error = ({
   error,
   reset,
 }: {
   error: Error & { digest?: string };
   reset: () => void;
-}) {
+}) => {
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <main className="absolute flex flex-col inset-0 items-center justify-center">
-      <h2 className="text-center">Something went wrong!</h2>
+    <main className="flex flex-col inset-0 items-center justify-center min-h-screen -mt-[130px]">
+      <h2 className="text-center dark:text-white">Something went wrong!</h2>
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
         onClick={() => reset()}>
@@ -23,4 +23,6 @@ export default function Error({
       </button>
     </main>
   );
-}
+};
+
+export default Error;

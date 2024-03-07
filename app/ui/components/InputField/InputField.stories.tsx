9170ef7ba1e458a002d6b6c461e51940292ce29e
Thao Pham Duy <thao.phamduy@asnet.com.vn>
2024-03-07 16:34:42 +0700

    fix(701): remove id and labe args in input field stories

--- app/ui/components/InputField/InputField.stories.tsx
+++ app/ui/components/InputField/InputField.stories.tsx
@@ -8,10 +8,6 @@ const meta = {
   component: InputField,
   tags: ["autodocs"],
   argTypes: {
-    id: {
-      description: "Id of input field",
-    },
-    label: { description: "Label of input field" },
     type: { description: "Type of input field" },
   },
 } as Meta<typeof InputField>;
@@ -22,16 +18,12 @@ type Story = StoryObj<typeof meta>;
 
 export const InputFieldTextType: Story = {
   args: {
-    id: "edit-fb",
-    label: "Facebook Account",
     type: "text",
   },
 };
 
 export const InputFieldNumberType: Story = {
   args: {
-    id: "add-product-price",
-    label: "Price",
     type: "number",
   },
 };

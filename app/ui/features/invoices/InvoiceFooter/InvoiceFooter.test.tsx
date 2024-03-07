746edcbe8536ae8a0a06020a9472fdfefb8c738a
Ngan.Do <ngan.do@asnet.com.vn>
2024-03-07 14:05:17 +0700

    fix(594): write unit test InvoiceFooter

--- app/ui/features/invoices/InvoiceFooter/InvoiceFooter.test.tsx
+++ app/ui/features/invoices/InvoiceFooter/InvoiceFooter.test.tsx
@@ -1,4 +1,5 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 // Components
 import InvoiceFooter from "./InvoiceFooter";
@@ -8,5 +9,31 @@ describe("InvoiceFooter Testing", () => {
     const { container } = render(<InvoiceFooter />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it("should renders invoice footer with correct content", () => {
+    const { getByText } = render(<InvoiceFooter />);
+
+    expect(getByText(/thank you!/i)).toBeInTheDocument();
+    expect(
+      getByText(
+        /if you encounter any issues related to the invoice you can contact us at:/i,
+      ),
+    ).toBeInTheDocument();
+    expect(getByText(/email:/i)).toBeInTheDocument();
+  });
+
+  it("prints invoice when print button is clicked", () => {
+    window.print = jest.fn();
+
+    const { getByRole } = render(<InvoiceFooter />);
+
+    fireEvent.click(
+      getByRole("button", {
+        name: /print/i,
+      }),
+    );
+
+    expect(window.print).toHaveBeenCalled();
   });
 });

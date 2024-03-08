bcb5293278535e44d967161724f6fc7653389ec9
Van Nguyen <van.nguyenthi@asnet.com.vn>
2024-03-08 18:13:56 +0700

    feat(263): use text tremor instead tag p for text on product page

--- app/(dashboard)/(products)/new-product/page.tsx
+++ app/(dashboard)/(products)/new-product/page.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 
 // Components
+import { Text } from "@tremor/react";
 const AddProductForm = dynamic(
   () => import("@/ui/features/products/Forms/AddProductForm"),
 );
@@ -13,9 +14,9 @@ const AddProductPage = () => {
         <h3 className="text-3xl font-bold text-[#344767] dark:text-white text-center mb-2">
           Add New Product
         </h3>
-        <p className="text-tremor-primary text-secondary text-center">
+        <Text className="text-xl dark:text-secondary text-secondary text-center">
           This information will describe more about the product.
-        </p>
+        </Text>
       </div>
       <AddProductForm />
     </div>

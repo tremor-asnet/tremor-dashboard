7e6846ec3986a0931982d5aa295bafd38099aea0
Ngan.Do <ngan.do@asnet.com.vn>
2024-03-08 14:56:13 +0700

    fix(630): remove redandunt code

--- app/ui/features/products/index.ts
+++ app/ui/features/products/index.ts
@@ -2,7 +2,6 @@ export { default as ImagePreview } from "./ImagePreview/ImagePreview";
 export { default as OtherProductList } from "./OtherProductList/OtherProductList";
 export { default as OtherProducts } from "./OtherProducts/OtherProducts";
 export { default as ProductDetail } from "./ProductDetail/ProductDetail";
-export { default as ProductFilter } from "./ProductFilter";
 export { default as ProductInfoDetail } from "./ProductInfoDetail/ProductInfoDetail";
 export { default as OtherProductSkeleton } from "./Skeleton/OtherProducts/OtherProductSkeleton";
 export { default as ProductDetailsSkeleton } from "./Skeleton/ProductDetails/ProductDetailsSkeleton";

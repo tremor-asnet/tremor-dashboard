a82c4d0856556efe517ba40d097223de913fd7c4
Ngan.Do <ngan.do@asnet.com.vn>
2024-03-12 23:21:42 +0700

    feat(592): write unit test for ProductInfo component

--- app/ui/features/products/EditProduct/ProductInfo/ProductInfo.test.tsx
+++ app/ui/features/products/EditProduct/ProductInfo/ProductInfo.test.tsx
@@ -1,12 +1,86 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 // Components
 import ProductInfo from "./ProductInfo";
 
+// Constants
+import { EXCEPT_KEYS } from "@/constants/common";
+
+jest.mock("react-hook-form", () => {
+  let mockReactHookForm = jest.requireActual("react-hook-form");
+
+  mockReactHookForm.useFormContext = jest.fn();
+  return {
+    ...mockReactHookForm,
+    useFormContext: jest.fn(() => {
+      return {
+        formState: {
+          errors: {},
+        },
+      };
+    }),
+    Controller: (props: any) => {
+      return (
+        <>
+          {props.render({
+            field: {},
+            formState: {
+              errors: {},
+            },
+          })}
+        </>
+      );
+    },
+  };
+});
+
 describe("Testing ProductInfo component", () => {
-  // TODO: Update unit test later
-  it.skip("Should match snapshot", () => {
+  const handleOnKeyDown = jest.fn();
+
+  it("should match snapshot", () => {
     const component = render(<ProductInfo />);
     expect(component).toMatchSnapshot();
+  });
+
+  it("should render title correctly", () => {
+    const { getByText } = render(<ProductInfo />);
+    expect(getByText("Product Information")).toBeInTheDocument();
+  });
+
+  it("renders pricing information fields correctly", () => {
+    const { getByText, getByLabelText } = render(<ProductInfo />);
+
+    expect(getByLabelText("Name")).toBeInTheDocument();
+    expect(getByText("Description")).toBeInTheDocument();
+    expect(getByLabelText("Weight")).toBeInTheDocument();
+    expect(getByLabelText("Quantity")).toBeInTheDocument();
+    expect(getByLabelText("Provider Name")).toBeInTheDocument();
+  });
+
+  it("should call preventDefault when key is in EXCEPT_KEYS.POSITIVE_DOUBLE", async () => {
+    const mockEvent = {
+      key: EXCEPT_KEYS.POSITIVE_DOUBLE[0],
+      preventDefault: jest.fn(),
+    };
+    const { getByRole } = render(<ProductInfo />);
+    const weightInput = getByRole("spinbutton", {
+      name: /weight/i,
+    });
+
+    fireEvent.keyDown(weightInput, mockEvent); // Invalid input
+
+    expect(weightInput).toBeInTheDocument();
+  });
+
+  it("should not call preventDefault when key is not in EXCEPT_KEYS.POSITIVE_DOUBLE", () => {
+    const mockEvent = {
+      key: "b",
+      preventDefault: jest.fn(),
+    };
+
+    handleOnKeyDown(mockEvent);
+
+    expect(mockEvent.preventDefault).not.toHaveBeenCalled();
   });
 });

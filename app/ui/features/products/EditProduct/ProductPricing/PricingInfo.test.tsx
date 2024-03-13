5aff113e7f64100f898e5e02ab83bcbfa079fe1b
Thang Ho Quang <thang.hoquang@asnet.com.vn>
2024-03-13 17:22:27 +0700

    fix(593): update unit test for ProductPricing component

--- app/ui/features/products/EditProduct/ProductPricing/PricingInfo.test.tsx
+++ app/ui/features/products/EditProduct/ProductPricing/PricingInfo.test.tsx
@@ -7,62 +7,37 @@ import PricingInfo from "./PricingInfo";
 // Constants
 import { EXCEPT_KEYS } from "@/constants/common";
 
-jest.mock("react-hook-form", () => {
-  let mockReactHookForm = jest.requireActual("react-hook-form");
-
-  mockReactHookForm.useFormContext = jest.fn();
-  return {
-    ...mockReactHookForm,
-    useFormContext: jest.fn(() => {
-      return {
-        formState: {
-          errors: {},
-        },
-      };
+jest.mock("react-hook-form", () => ({
+  ...jest.requireActual("react-hook-form"),
+  // Mock the useFormContext hook
+  useFormContext: jest.fn(() => {
+    return {
+      formState: {
+        errors: {},
+      },
+    };
+  }),
+  // Mock the Controller component
+  Controller: (props: any) =>
+    props.render({
+      field: {
+        value: [],
+      },
+      formState: {
+        errors: {},
+      },
     }),
-    Controller: (props: any) => {
-      return (
-        <>
-          {props.render({
-            field: {
-              value: [],
-            },
-            formState: {
-              errors: {},
-            },
-          })}
-        </>
-      );
-    },
-  };
-});
+}));
 
 describe("Testing PricingInfo component", () => {
-  const handleOnKeyDown = jest.fn();
-
   it("Should match snapshot", () => {
     const component = render(<PricingInfo />);
     expect(component).toMatchSnapshot();
   });
 
-  it("should render title correctly", () => {
-    const { getByText } = render(<PricingInfo />);
-    expect(getByText("Pricing")).toBeInTheDocument();
-  });
-
-  it("renders pricing information fields correctly", () => {
-    const { getByText, getByLabelText } = render(<PricingInfo />);
-
-    expect(getByLabelText("Price")).toBeInTheDocument();
-    expect(getByText("Currency")).toBeInTheDocument();
-    expect(getByLabelText("SKU")).toBeInTheDocument();
-    expect(getByText("Tags")).toBeInTheDocument();
-  });
-
-  it("should call preventDefault when key is in except key", async () => {
+  it("Should call preventDefault when key is in except key", async () => {
     const mockEvent = {
       key: EXCEPT_KEYS.POSITIVE_DOUBLE[0],
-      preventDefault: jest.fn(),
     };
     const { getByRole } = render(<PricingInfo />);
     const priceInput = getByRole("spinbutton", {
@@ -72,16 +47,5 @@ describe("Testing PricingInfo component", () => {
     fireEvent.keyDown(priceInput, mockEvent); // Invalid input
 
     expect(priceInput).toBeInTheDocument();
-  });
-
-  it("should not call preventDefault when key is not in except key", () => {
-    const mockEvent = {
-      key: "b",
-      preventDefault: jest.fn(),
-    };
-
-    handleOnKeyDown(mockEvent);
-
-    expect(mockEvent.preventDefault).not.toHaveBeenCalled();
   });
 });

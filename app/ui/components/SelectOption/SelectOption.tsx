e4fcd65ee1c83c46cbb1ac19fd36749d5cc9f4c3
Ngan.Do <ngan.do@asnet.com.vn>
2024-03-19 17:22:51 +0700

    fix(703): move this function out of the render element

--- app/ui/components/SelectOption/SelectOption.tsx
+++ app/ui/components/SelectOption/SelectOption.tsx
@@ -20,36 +20,40 @@ const SelectOption = ({
   title,
   onSelectItem,
   onSelectRemove,
-}: SelectOptionProps) => (
-  <>
-    <ul>
-      {data.map(({ option, value }) => {
-        const handleSelectItem = () => onSelectItem(option, value);
-        return (
-          <li
-            className="w-full text-tremor-default cursor-pointer text-secondary px-4 py-[0.3rem] hover:bg-body hover:text-tremor-brand-subtle hover:rounded-md min-h-[auto] dark:text-dark-romance dark:hover:bg-dark-secondary"
-            key={option}>
-            <Button
-              onClick={handleSelectItem}
-              value={value}
-              variant={VARIANT_BUTTON.LIGHT}
-              data-testid={`item-filter-${value}`}
-              className="text-secondary hover:text-primary dark:text-lighter dark:hover:text-lighter">
-              {title}: {option}
-            </Button>
-          </li>
-        );
-      })}
-    </ul>
-    <div className="h-px bg-gradient-select my-2 opacity-25 dark:bg-gradient-divider" />
-    <Button
-      data-testid="remove-filter"
-      className="w-full text-tremor-default hover:text-attention cursor-pointer justify-start text-attention dark:text-attention dark:hover:ttext-attention px-4 py-[0.3rem] hover:bg-body hover:rounded-md min-h-[auto] dark:hover:bg-dark-secondary text-left"
-      onClick={onSelectRemove}
-      variant={VARIANT_BUTTON.LIGHT}>
-      Remove Filter
-    </Button>
-  </>
-);
+}: SelectOptionProps) => {
+  const handleSelectItem = (option: string, value: string) =>
+    onSelectItem(option, value);
+
+  return (
+    <>
+      <ul>
+        {data.map(({ option, value }) => {
+          return (
+            <li
+              className="w-full text-tremor-default cursor-pointer text-secondary px-4 py-[0.3rem] hover:bg-body hover:text-tremor-brand-subtle hover:rounded-md min-h-[auto] dark:text-dark-romance dark:hover:bg-dark-secondary"
+              key={option}>
+              <Button
+                onClick={() => handleSelectItem(option, value)}
+                value={value}
+                variant={VARIANT_BUTTON.LIGHT}
+                data-testid={`item-filter-${value}`}
+                className="text-secondary hover:text-primary dark:text-lighter dark:hover:text-lighter">
+                {title}: {option}
+              </Button>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="h-px bg-gradient-select my-2 opacity-25 dark:bg-gradient-divider" />
+      <Button
+        data-testid="remove-filter"
+        className="w-full text-tremor-default hover:text-attention cursor-pointer justify-start text-attention dark:text-attention dark:hover:ttext-attention px-4 py-[0.3rem] hover:bg-body hover:rounded-md min-h-[auto] dark:hover:bg-dark-secondary text-left"
+        onClick={onSelectRemove}
+        variant={VARIANT_BUTTON.LIGHT}>
+        Remove Filter
+      </Button>
+    </>
+  );
+};
 
 export default memo(SelectOption);

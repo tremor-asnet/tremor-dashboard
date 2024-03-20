9c8cc399f7dd4489cdb3b426349d586c15411956
Ngan.Do <ngan.do@asnet.com.vn>
2024-03-20 10:46:54 +0700

    fix(703): remove redundant code

--- app/ui/components/SelectOption/SelectOption.tsx
+++ app/ui/components/SelectOption/SelectOption.tsx
@@ -1,12 +1,12 @@
 // Types
 import { OptionType } from "@/types";
+import { memo } from "react";
 
 // Components
 import { Button } from "@tremor/react";
 
 // Constants
 import { VARIANT_BUTTON } from "@/constants";
-import { memo } from "react";
 
 interface SelectOptionProps {
   title: string;
@@ -27,22 +27,20 @@ const SelectOption = ({
   return (
     <>
       <ul>
-        {data.map(({ option, value }) => {
-          return (
-            <li
-              className="w-full text-tremor-default cursor-pointer text-secondary px-4 py-[0.3rem] hover:bg-body hover:text-tremor-brand-subtle hover:rounded-md min-h-[auto] dark:text-dark-romance dark:hover:bg-dark-secondary"
-              key={option}>
-              <Button
-                onClick={handleSelectItem(option, value)}
-                value={value}
-                variant={VARIANT_BUTTON.LIGHT}
-                data-testid={`item-filter-${value}`}
-                className="text-secondary hover:text-primary dark:text-lighter dark:hover:text-lighter">
-                {title}: {option}
-              </Button>
-            </li>
-          );
-        })}
+        {data.map(({ option, value }) => (
+          <li
+            className="w-full text-tremor-default cursor-pointer text-secondary px-4 py-[0.3rem] hover:bg-body hover:text-tremor-brand-subtle hover:rounded-md min-h-[auto] dark:text-dark-romance dark:hover:bg-dark-secondary"
+            key={option}>
+            <Button
+              onClick={handleSelectItem(option, value)}
+              value={value}
+              variant={VARIANT_BUTTON.LIGHT}
+              data-testid={`item-filter-${value}`}
+              className="text-secondary hover:text-primary dark:text-lighter dark:hover:text-lighter">
+              {title}: {option}
+            </Button>
+          </li>
+        ))}
       </ul>
       <div className="h-px bg-gradient-select my-2 opacity-25 dark:bg-gradient-divider" />
       <Button

27faaaa2eea52f6185d1883a7f74e4690b736148
du.nguyen <du.nguyen@asnet.com.vn>
2024-03-20 14:29:26 +0700

    feat(747): remove redundant code and update naming

--- app/ui/components/Modal/index.tsx
+++ app/ui/components/Modal/index.tsx
@@ -1,29 +1,22 @@
 "use client";
 
-import React, {
-  ElementRef,
-  ReactNode,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
 import { createPortal } from "react-dom";
+import React, { ElementRef, ReactNode, useRef, useState } from "react";
 
-import { Button, Flex } from "@tremor/react";
 import { GrClose } from "react-icons/gr";
+import { Button, Flex } from "@tremor/react";
 
 import { twMerge } from "tailwind-merge";
 
 interface IModal {
   title: string;
   children: ReactNode;
-  className?: string;
-  showCloseButton?: boolean;
+  additionalClasses?: string;
+  showCloseIconBtn?: boolean;
   open?: boolean;
-  onConfirm?: () => void;
-  labelClose?: string;
-  labelPrimary?: string;
-  labelSecondary?: string;
+  labelCloseBtn?: string;
+  labelPrimaryBtn?: string;
+  labelSecondaryBtn?: string;
   onClose?: () => void;
   onPrimaryBtn?: () => void;
   onSecondaryBtn?: () => void;
@@ -32,12 +25,12 @@ interface IModal {
 export default function Modal({
   title,
   children,
-  className = "",
+  additionalClasses = "",
   open = true,
-  showCloseButton = false,
-  labelClose = "Cancel",
-  labelPrimary = "Submit",
-  labelSecondary = "Done",
+  showCloseIconBtn = false,
+  labelCloseBtn = "Cancel",
+  labelPrimaryBtn = "Submit",
+  labelSecondaryBtn = "Done",
 
   onPrimaryBtn,
   onSecondaryBtn,
@@ -46,14 +39,7 @@ export default function Modal({
   const [isOpen, setOpen] = useState(open);
   const dialogRef = useRef<ElementRef<"dialog">>(null);
 
-  useEffect(() => {
-    if (!dialogRef.current?.open) {
-      dialogRef.current?.showModal();
-      setOpen(true);
-    }
-  }, []);
-
-  const handleDismiss = () => {
+  const handleClose = () => {
     dialogRef.current?.close();
     setOpen(false);
     onClose?.();
@@ -61,12 +47,12 @@ export default function Modal({
 
   const handlePrimary = () => {
     onPrimaryBtn?.();
-    handleDismiss();
+    handleClose();
   };
 
   const handleSecondary = () => {
     onSecondaryBtn?.();
-    handleDismiss();
+    handleClose();
   };
 
   if (!isOpen) {
@@ -80,20 +66,21 @@ export default function Modal({
       alignItems="center">
       <dialog
         ref={dialogRef}
-        onClose={handleDismiss}
+        onClose={handleClose}
+        open={isOpen}
         className={twMerge(
           "py-5 px-4 rounded-md border-none outline-none flex flex-col gap-5 sm:min-w-[80%] md:min-w-[500px] bg-tremor-primary dark:bg-dark-tremor-primary",
-          className,
+          additionalClasses,
         )}>
         <h2 className="font-bold text-2xl text-center text-tertiary dark:text-dark-romance">
           {title}
         </h2>
 
-        {showCloseButton && (
+        {showCloseIconBtn && (
           <Button
             type="button"
             variant="light"
-            onClick={handleDismiss}
+            onClick={handleClose}
             className="absolute top-5 right-5 ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 outline-none border-none rounded-full focus:ring-2 focus:ring-gray-300 p-3 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
             aria-label="Close">
             <GrClose className="h-5 w-5 shrink-0 text-xl text-tertiary dark:text-white" />
@@ -109,7 +96,7 @@ export default function Modal({
               className={twMerge(
                 "bg-green-500 dark:bg-green-500 hover:bg-green-500 dark:hover:bg-green-500 text-white dark:text-white outline-none border-none px-6 py-4 text-xl font-bold flex-1",
               )}>
-              {labelPrimary}
+              {labelPrimaryBtn}
             </Button>
           )}
 
@@ -119,16 +106,16 @@ export default function Modal({
               className={twMerge(
                 "text-white dark:text-white outline-none border-none px-6 py-4 text-xl font-bold flex-1",
               )}>
-              {labelSecondary}
+              {labelSecondaryBtn}
             </Button>
           )}
 
           <Button
-            onClick={handleDismiss}
+            onClick={handleClose}
             className={twMerge(
               "bg-orange-500 dark:bg-orange-500 hover:bg-orange-500 dark:hover:bg-orange-500 text-white dark:text-white outline-none border-none px-6 py-4 text-xl font-bold flex-1",
             )}>
-            {labelClose}
+            {labelCloseBtn}
           </Button>
         </Flex>
       </dialog>

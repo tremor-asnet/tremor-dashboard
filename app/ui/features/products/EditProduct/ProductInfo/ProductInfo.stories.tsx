729dc45bc99c02ac560e3e5d272bfea69b727225
Ngan.Do <ngan.do@asnet.com.vn>
2024-03-21 17:01:47 +0700

    fix(735): remove the comment code

--- app/ui/features/products/EditProduct/ProductInfo/ProductInfo.stories.tsx
+++ app/ui/features/products/EditProduct/ProductInfo/ProductInfo.stories.tsx
@@ -1,10 +1,9 @@
 // Libs
 import { ReactNode, FC } from "react";
-import { FormProvider, useForm, useFormContext } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import type { Meta, StoryObj } from "@storybook/react";
 
 // Components
-import { Flex } from "@tremor/react";
 import ProductInfo from "./ProductInfo";
 
 const StorybookFormProvider: FC<{ children: ReactNode }> = ({ children }) => {
@@ -21,26 +20,6 @@ const meta = {
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-
-// export const NewProductInfo: Story = {
-//   render: () => (
-//     <StorybookFormProvider>
-//       <Flex className="w-full justify-center mt-10">
-//         <ProductInfo />
-//       </Flex>
-//     </StorybookFormProvider>
-//   ),
-// };
-
-// export const EditProductInfo: Story = {
-//   render: () => (
-//     <StorybookFormProvider>
-//       <Flex className="w-full justify-center mt-10">
-//         <ProductInfo />
-//       </Flex>
-//     </StorybookFormProvider>
-//   ),
-// };
 
 export const Primary: Story = {
   render: () => (

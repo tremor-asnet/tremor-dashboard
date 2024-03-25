29d1f6c4cc75313182eaa66f3c30f6f1bbf0e0d8
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2024-03-25 11:10:39 +0700

    Feat/show hide card number

--- app/ui/features/billing/BillingCard/BillingCard.tsx
+++ app/ui/features/billing/BillingCard/BillingCard.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-// Libs
-import { Flex, Text, Title } from "@tremor/react";
+import { useState } from "react";
 import Image from "next/image";
+
+// Components
+import { Button, Flex, Text, Title } from "@tremor/react";
 
 //Icons
 import { MdWifi } from "react-icons/md";
+import { FaRegEye } from "react-icons/fa6";
+import { FaRegEyeSlash } from "react-icons/fa6";
 
 // Constants
 import { METADATA } from "@/constants";
@@ -17,26 +21,42 @@ const BillingCard = ({
   cardNumber,
   holderFullName,
   expire,
+  cardLast4Digit,
 }: {
   cardNumber: string;
   holderFullName: string;
   expire: string;
+  cardLast4Digit: string;
 }) => {
+  const [isShowAmount, setIsShowAmount] = useState(false);
+
+  const toggleShowHide = () => {
+    setIsShowAmount(!isShowAmount);
+  };
+
   return (
     <Flex
       alignItems="center"
       className="relative text-white bg-gradient-primary dark:bg-gradient-pickled p-0 rounded-lg min-h-[236px] shadow-[rgba(0,0,0,0.1)_0rem_1.25rem_1.5625rem_-0.3125rem,rgba(0,0,0,0.04)_0rem_0.625rem_0.625rem_-0.3125rem]">
-      <div className="w-full h-full absolute top-0 left-0 opacity-20 bg-cover bg-billing-card"></div>
+      <div className="w-full h-full absolute top-0 left-0 opacity-20 bg-cover bg-billing-card z-0"></div>
       <Flex
         flexDirection="col"
         justifyContent="start"
         alignItems="center"
-        className="p-4">
+        className="p-4 z-10">
         <Flex>
           <MdWifi className="text-2xl m-2" />
         </Flex>
-        <Flex className="text-xl text-white font-bold mt-6 mb-10">
-          {cardNumber}
+        <Flex className="mt-6 mb-8 gap-4">
+          <Text className="text-xl text-white font-bold">
+            {isShowAmount ? cardNumber : `**** **** **** ${cardLast4Digit}`}
+          </Text>
+          <Button
+            variant="light"
+            icon={isShowAmount ? FaRegEyeSlash : FaRegEye}
+            onClick={toggleShowHide}
+            className="text-white hover:text-white dark:text-white dark:hover:text-white"
+          />
         </Flex>
         <Flex className="2xl:py-[5px]">
           <Flex>

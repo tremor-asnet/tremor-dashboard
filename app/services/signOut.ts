079524c4610e2d512612a4276734c4ff5fba48d8
du.nguyen <du.nguyen@asnet.com.vn>
2024-03-26 17:15:45 +0700

    fix(822): catch error for handle signout

--- app/services/signOut.ts
+++ app/services/signOut.ts
@@ -1,3 +1,11 @@
 "use client";
 
-export const signOut = () => fetch(`/api/logout`, { method: "POST" });
+import { getErrorMessage } from "@/helpers";
+
+export const signOut = async () => {
+  const res = await fetch(`/api/logout`, { method: "POST" });
+
+  if (!res.ok) {
+    throw new Error(getErrorMessage(res.status, res.statusText));
+  }
+};

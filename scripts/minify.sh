2e6fe1821bf06601d610689d11e555bc655bd82c
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2024-03-27 16:18:20 +0700

    feat(827): update minify script

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -1,12 +1,10 @@
-
 echo 'Minify middleware';
 npx terser .next/server/middleware.js -o .next/server/middleware.js
 
 echo 'Minify Static';
-npx terser .next/static/chunks/pages/*.js -o .next/static/chunks/app/*.js
+npx terser .next/static/chunks/pages/*.js -o .next/static/chunks/pages/*.js
 npx terser .next/static/chunks/app/*.js -o .next/static/chunks/app/*.js
 
-
 echo 'Minify Server';
-npx terser .next/server/pages/*.js -o .next/server/pages/*.js
 npx terser .next/server/app/**/*.js -o .next/server/app/**/*.js
+npx terser .next/server/pages/*.js -o .next/server/pages/*.js
